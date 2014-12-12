8bb7237896bf2bb4330989bcda94e57061684f7a
Ryan Crichton <rg.crichton@gmail.com>
2014-12-12 16:12:33 +0200

    Changed openhim-core binary to use forever and write to stdout and stderr. We should write a upstart config that make sue of this binary.

--- bin/openhim-core.js
+++ bin/openhim-core.js
@@ -1,34 +1,26 @@
 #!/usr/bin/env node
-(function () {
-  //require('../lib/server').start();
+var forever = require('forever');
+var path = require('path');
+var fs   = require('fs');
 
-  var forever = require('forever');
+var lib  = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
 
-  var path = require('path');
-  var fs   = require('fs');
-  var lib  = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
+var child = new (forever.Monitor)('server.js', {
+  sourceDir: lib,
+  command: 'node --harmony',
+  watch: true
+});
 
-  var child = new (forever.Monitor)('server.js', {
-    sourceDir: lib
-  });
+child.on('watch:restart', function(info) {
+    console.error('Restaring script because ' + info.file + ' changed');
+});
 
-  console.log('Args: ' + process.argv);
+child.on('restart', function() {
+    console.error('Forever restarting script for ' + child.times + ' time');
+});
 
-  child.on('watch:restart', function(info) {
-      console.error('Restaring script because ' + info.file + ' changed');
-  });
+child.on('exit:code', function(code) {
+    console.error('Forever detected script exited with code ' + code);
+});
 
-  child.on('restart', function() {
-      console.error('Forever restarting script for ' + child.times + ' time');
-  });
-
-  child.on('exit:code', function(code) {
-      console.error('Forever detected script exited with code ' + code);
-  });
-
-  forever.startDaemon(child);
-  child.start();
-
-  console.log('HERE');
-
-}());
+child.start();
