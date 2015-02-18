0174c8d47b9d5d907d04dac3265f60143ec407f3
Carl Leitner <litlfred@gmail.com>
2015-02-18 11:27:35 -0500

    "Release Version 1.0.0-30"

---
+++ packaging/targets/trusty/home/openhim/bin/install_node.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+USERNAME=openhim
+HOME=/home/$USERNAME
+NPM=/usr/bin/npm
+CURL=/usr/bin/curl
+COREDIR=$HOME/openhim-core-js
+SH=/bin/sh
+
+cd $HOME
+
+
+nvm install 0.12
+nvm alias default 0.12
+nvm use 0.12
+
+cd $COREDIR
+
+$NPM install 
+$NPM install -g grunt-cli grunt
+#  grunt-coffeelint grunt-contrib-coffee grunt-contrib-clean grunt-mocha-cli grunt-contrib-watch
+
+grunt build
