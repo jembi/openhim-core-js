0246501b7e7d0f497f71f55fb541ab36ee473aa7
Ryan Crichton <rg.crichton@gmail.com>
2015-08-26 15:35:39 +0200

    Updated packaging to work from a clean ubuntu install.

--- packaging/targets/trusty/home/openhim/bin/install_openhim.sh
+++ packaging/targets/trusty/home/openhim/bin/install_openhim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 OPENHIM_VERSION=
 
@@ -10,10 +11,12 @@ SH=/bin/bash
 cd $HOME
 
 # Install NVM
-$CURL -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | $SH
+echo "Installing node version manager for "$USERNAME" user ..."
+$CURL -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | $SH > /dev/null
 . $HOME/.nvm/nvm.sh
 
 # Install node 0.12
+echo "Installing node.js 0.12.7 via nvm ..."
 nvm install 0.12.7
 nvm alias default 0.12.7
 nvm use 0.12.7
@@ -21,3 +24,6 @@ nvm use 0.12.7
 # Install OpenHIM, globally
 echo "Installing OpenHIM v"$OPENHIM_VERSION" using npm ("`which npm`") ..."
 npm install openhim-core@$OPENHIM_VERSION -g
+echo "OpenHIM v"$OPENHIM_VERSION" installed!"
+
+exit 0
