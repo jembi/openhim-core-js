12ac97beee9aea3b7c09428f4c9c313155746a7c
Andrew Ross <andrew.ross@jembi.org>
2017-06-05 13:38:55 +0200

    Converted metrics

--- src/metrics.js
+++ src/metrics.js
@@ -1,7 +1,7 @@
 // TODO: This file was created by bulk-decaffeinate.
 // Sanity-check the conversion and remove this comment.
-import { Transaction } from './model/transactions';
-import logger from 'winston';
+import logger from "winston";
+import { Transaction } from "./model/transactions";
 
 // Calculates transaction metrics
 //
@@ -62,90 +62,100 @@ import logger from 'winston';
 //   }
 // ]
 export function calculateMetrics(startDate, endDate, transactionFilter, channelIDs, timeSeries, groupByChannels) {
-  if (!(startDate instanceof Date) || !(endDate instanceof Date)) {
-    return new Promise(function(resolve, reject) {
-      return reject(new Error('startDate and endDate must be provided and be of type Date'));
-    });
-  }
+	if (!(startDate instanceof Date) || !(endDate instanceof Date)) {
+		return new Promise((resolve, reject) => {
+			return reject(new Error("startDate and endDate must be provided and be of type Date"));
+		});
+	}
 
-  let match = {
-    "request.timestamp": {
-      $lt: endDate,
-      $gt: startDate
-    }
-  };
-  if (transactionFilter) {
-    Object.assign(match, transactionFilter);
-  }
+	const match = {
+		"request.timestamp": {
+			$lt: endDate,
+			$gt: startDate,
+		},
+	};
+	if (transactionFilter) {
+		Object.assign(match, transactionFilter);
+	}
 
-  if (channelIDs) {
-    match.channelID =
-      {$in: channelIDs};
-  }
+	if (channelIDs) {
+		match.channelID =
+			{ $in: channelIDs };
+	}
 
-  let group = {
-    _id: {},
-    total: { $sum: 1
-  },
-    avgResp: { $avg: {$subtract: [ "$response.timestamp", "$request.timestamp" ]}
-  },
-    minResp: { $min: {$subtract: [ "$response.timestamp", "$request.timestamp" ]}
-  },
-    maxResp: { $max: {$subtract: [ "$response.timestamp", "$request.timestamp" ]}
-  },
-    failed: { $sum: {$cond: [ {$eq: [ "$status", "Failed" ]}, 1, 0 ]}
-  },
-    successful: { $sum: {$cond: [ {$eq: [ "$status", "Successful" ]}, 1, 0 ]}
-  },
-    processing: { $sum: {$cond: [ {$eq: [ "$status", "Processing" ]}, 1, 0 ]}
-  },
-    completed: { $sum: {$cond: [ {$eq: [ "$status", "Completed" ]}, 1, 0 ]}
-  },
-    completedWErrors: { $sum: {$cond: [ {$eq: [ "$status", "Completed with error(s)" ]}, 1, 0 ]}
-  }
-  };
-  if (groupByChannels) {
-    group._id.channelID = '$channelID';
-  }
+	const group = {
+		_id: {},
+		total: {
+			$sum: 1,
+		},
+		avgResp: {
+			$avg: { $subtract: ["$response.timestamp", "$request.timestamp"] },
+		},
+		minResp: {
+			$min: { $subtract: ["$response.timestamp", "$request.timestamp"] },
+		},
+		maxResp: {
+			$max: { $subtract: ["$response.timestamp", "$request.timestamp"] },
+		},
+		failed: {
+			$sum: { $cond: [{ $eq: ["$status", "Failed"] }, 1, 0] },
+		},
+		successful: {
+			$sum: { $cond: [{ $eq: ["$status", "Successful"] }, 1, 0] },
+		},
+		processing: {
+			$sum: { $cond: [{ $eq: ["$status", "Processing"] }, 1, 0] },
+		},
+		completed: {
+			$sum: { $cond: [{ $eq: ["$status", "Completed"] }, 1, 0] },
+		},
+		completedWErrors: {
+			$sum: { $cond: [{ $eq: ["$status", "Completed with error(s)"] }, 1, 0] },
+		},
+	};
+	if (groupByChannels) {
+		group._id.channelID = "$channelID";
+	}
 
-  if (timeSeries) {
-    switch (timeSeries) {
-      case "minute":
-        group._id.minute = {$minute: "$request.timestamp"};
-        group._id.hour = {$hour: "$request.timestamp"};
-        group._id.day = {$dayOfMonth: "$request.timestamp"};
-        group._id.week = {$week: "$request.timestamp"};
-        group._id.month = {$month: "$request.timestamp"};
-        group._id.year = {$year: "$request.timestamp"};
-        break;
-      case "hour":
-        group._id.hour = {$hour: "$request.timestamp"};
-        group._id.week = {$week: "$request.timestamp"};
-        group._id.day = {$dayOfMonth: "$request.timestamp"};
-        group._id.month = {$month: "$request.timestamp"};
-        group._id.year = {$year: "$request.timestamp"};
-        break;
-      case "day":
-        group._id.day = {$dayOfMonth: "$request.timestamp"};
-        group._id.week = {$week: "$request.timestamp"};
-        group._id.month = {$month: "$request.timestamp"};
-        group._id.year = {$year: "$request.timestamp"};
-        break;
-      case "week":
-        group._id.week = {$week: "$request.timestamp"};
-        group._id.month = {$month: "$request.timestamp"};
-        group._id.year = {$year: "$request.timestamp"};
-        break;
-      case "month":
-        group._id.month = {$month: "$request.timestamp"};
-        group._id.year = {$year: "$request.timestamp"};
-        break;
-      case "year":
-        group._id.year = {$year: "$request.timestamp"};
-        break;
-    }
-  }
+	if (timeSeries) {
+		switch (timeSeries) {
+			case "minute":
+				group._id.minute = { $minute: "$request.timestamp" };
+				group._id.hour = { $hour: "$request.timestamp" };
+				group._id.day = { $dayOfMonth: "$request.timestamp" };
+				group._id.week = { $week: "$request.timestamp" };
+				group._id.month = { $month: "$request.timestamp" };
+				group._id.year = { $year: "$request.timestamp" };
+				break;
+			case "hour":
+				group._id.hour = { $hour: "$request.timestamp" };
+				group._id.week = { $week: "$request.timestamp" };
+				group._id.day = { $dayOfMonth: "$request.timestamp" };
+				group._id.month = { $month: "$request.timestamp" };
+				group._id.year = { $year: "$request.timestamp" };
+				break;
+			case "day":
+				group._id.day = { $dayOfMonth: "$request.timestamp" };
+				group._id.week = { $week: "$request.timestamp" };
+				group._id.month = { $month: "$request.timestamp" };
+				group._id.year = { $year: "$request.timestamp" };
+				break;
+			case "week":
+				group._id.week = { $week: "$request.timestamp" };
+				group._id.month = { $month: "$request.timestamp" };
+				group._id.year = { $year: "$request.timestamp" };
+				break;
+			case "month":
+				group._id.month = { $month: "$request.timestamp" };
+				group._id.year = { $year: "$request.timestamp" };
+				break;
+			case "year":
+				group._id.year = { $year: "$request.timestamp" };
+				break;
+			default : break; // TODO: Check if this is valid
+		}
+	}
 
-  let pipeline = [ { $match: match }, { $group: group } ];
-  return Transaction.aggregate(pipeline).exec();
+	const pipeline = [{ $match: match }, { $group: group }];
+	return Transaction.aggregate(pipeline).exec();
 }
