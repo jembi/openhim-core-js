46396c905c32bad6464f26641fa5a36b43b08e70
Andrew Ross <andrew.ross@jembi.org>
2017-06-06 13:54:53 +0200

    Completed config, authorisation and basicAuth

--- src/middleware/authorisation.js
+++ src/middleware/authorisation.js
@@ -1,87 +1,84 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
 import Q from "q";
 import logger from "winston";
-import atna from 'atna-audit';
-import config from '../config/config';
-config.authentication = config.get('authentication');
-let utils = require('../utils');
-let auditing = require('../auditing');
+import atna from "atna-audit";
+import SDC from "statsd-client";
+import os from "os";
+import utils from "../utils";
+import auditing from "../auditing";
+import config from "../config/config";
 
-let statsdServer = config.get('statsd');
-let application = config.get('application');
-let himSourceID = config.get('auditing').auditEvents.auditSourceID;
-const SDC = require('statsd-client');
-let os = require('os');
+config.authentication = config.get("authentication");
+const statsdServer = config.get("statsd");
+const application = config.get("application");
+const himSourceID = config.get("auditing").auditEvents.auditSourceID;
 
-let domain = `${os.hostname()}.${application.name}.appMetrics`;
-let sdc = new SDC(statsdServer);
+const domain = `${os.hostname()}.${application.name}.appMetrics`;
+const sdc = new SDC(statsdServer);
 
- function genAuthAudit(remoteAddress) {
-  let audit = atna.nodeAuthentication(remoteAddress, himSourceID, os.hostname(), atna.OUTCOME_MINOR_FAILURE);
-  audit = atna.wrapInSyslog(audit);
-  return audit;
-};
-
- function authoriseClient(channel, ctx) {
-  if ((ctx.authenticated != null) && (channel.allow != null)) {
-    if (ctx.authenticated.roles != null) {
-      for (let role of Array.from(channel.allow)) {
-        if (Array.from(ctx.authenticated.roles).includes(role)) {
-          return true;
-        }
-      }
-    }
-    if (Array.from(channel.allow).includes(ctx.authenticated.clientID)) {
-      return true;
-    }
-  }
-
-  return false;
-};
-
- function authoriseIP(channel, ctx) {
-  if ((channel.whitelist != null ? channel.whitelist.length : undefined) > 0) {
-    return Array.from(channel.whitelist).includes(ctx.ip);
-  } else {
-    return true; // whitelist auth not required
-  }
-};
-
-export function authorise(ctx, done) {
-
-  let channel = ctx.matchingChannel;
-
-  if ((channel != null) && authoriseIP(channel, ctx) && ((channel.authType === 'public') || authoriseClient(channel, ctx))) {
-    // authorisation succeeded
-    ctx.authorisedChannel = channel;
-    logger.info(`The request, '${ctx.request.path}' is authorised to access ${ctx.authorisedChannel.name}`);
-  } else {
-    // authorisation failed
-    ctx.response.status = 401;
-    if (config.authentication.enableBasicAuthentication) {
-      ctx.set("WWW-Authenticate", "Basic");
-    }
-    logger.info(`The request, '${ctx.request.path}', is not authorised to access any channels.`);
-    auditing.sendAuditEvent(genAuthAudit(ctx.ip), () => logger.debug('Processed nodeAuthentication audit'));
-  }
-
-  return done();
+function genAuthAudit(remoteAddress) {
+	let audit = atna.nodeAuthentication(remoteAddress, himSourceID, os.hostname(), atna.OUTCOME_MINOR_FAILURE);
+	audit = atna.wrapInSyslog(audit);
+	return audit;
 }
 
-export function koaMiddleware(next) {
-  let startTime;
-  if (statsdServer.enabled) { startTime = new Date(); }
-  let authorise = Q.denodeify(exports.authorise);
-  ({}); //TODO:Fix yield authorise this
-  if (this.authorisedChannel != null) {
-    if (statsdServer.enabled) { sdc.timing(`${domain}.authorisationMiddleware`, startTime); }
-    return {}; //TODO:Fix yield next
-  }
+function authoriseClient(channel, ctx) {
+	if ((ctx.authenticated != null) && (channel.allow != null)) {
+		if (ctx.authenticated.roles != null) {
+			for (const role of Array.from(channel.allow)) {
+				if (Array.from(ctx.authenticated.roles).includes(role)) {
+					return true;
+				}
+			}
+		}
+		if (Array.from(channel.allow).includes(ctx.authenticated.clientID)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function authoriseIP(channel, ctx) {
+	if ((channel.whitelist != null ? channel.whitelist.length : undefined) > 0) {
+		return Array.from(channel.whitelist).includes(ctx.ip);
+	} else {
+		return true; // whitelist auth not required
+	}
+}
+
+export function authorise(ctx, done) {
+	const channel = ctx.matchingChannel;
+
+	if ((channel != null) && authoriseIP(channel, ctx) && ((channel.authType === "public") || authoriseClient(channel, ctx))) {
+		// authorisation succeeded
+		ctx.authorisedChannel = channel;
+		logger.info(`The request, '${ctx.request.path}' is authorised to access ${ctx.authorisedChannel.name}`);
+	} else {
+		// authorisation failed
+		ctx.response.status = 401;
+		if (config.authentication.enableBasicAuthentication) {
+			ctx.set("WWW-Authenticate", "Basic");
+		}
+		logger.info(`The request, '${ctx.request.path}', is not authorised to access any channels.`);
+		auditing.sendAuditEvent(genAuthAudit(ctx.ip), () => logger.debug("Processed nodeAuthentication audit"));
+	}
+
+	return done();
+}
+
+export function* koaMiddleware(next) {
+	let startTime;
+	if (statsdServer.enabled) { startTime = new Date(); }
+	const authorise = Q.denodeify(exports.authorise);
+	yield authorise(this);
+	if (this.authorisedChannel != null) {
+		if (statsdServer.enabled) { sdc.timing(`${domain}.authorisationMiddleware`, startTime); }
+		return yield next;
+	}
 }
 
 // export private functions for unit testing
 // note: you cant spy on these method because of this :(
 if (process.env.NODE_ENV === "test") {
-  exports.genAuthAudit = genAuthAudit;
+	exports.genAuthAudit = genAuthAudit;
 }
--- src/middleware/basicAuthentication.js
+++ src/middleware/basicAuthentication.js
@@ -1,98 +1,95 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-import auth from 'basic-auth';
+import auth from "basic-auth";
 import Q from "q";
-import { Client } from "../model/clients";
 import logger from "winston";
 import crypto from "crypto";
+import bcrypt from "bcryptjs";
+import SDC from "statsd-client";
+import os from "os";
+import { Client } from "../model/clients";
+import config from "../config/config";
 
-import bcrypt from 'bcryptjs';
+const statsdServer = config.get("statsd");
+const application = config.get("application");
 
-import config from '../config/config';
-let statsdServer = config.get('statsd');
-let application = config.get('application');
-const SDC = require('statsd-client');
-let os = require('os');
+const domain = `${os.hostname()}.${application.name}.appMetrics`;
+const sdc = new SDC(statsdServer);
 
-let domain = `${os.hostname()}.${application.name}.appMetrics`;
-let sdc = new SDC(statsdServer);
+const bcryptCompare = (pass, client, callback) => bcrypt.compare(pass, client.passwordHash, callback);
 
-let bcryptCompare = (pass, client, callback) => bcrypt.compare(pass, client.passwordHash, callback);
+function cryptoCompare(pass, client, callback) {
+	const hash = crypto.createHash(client.passwordAlgorithm);
+	hash.update(pass);
+	hash.update(client.passwordSalt);
+	if (hash.digest("hex") === client.passwordHash) {
+		return callback(null, true);
+	} else {
+		return callback(null, false);
+	}
+}
 
- function cryptoCompare(pass, client, callback) {
-  let hash = crypto.createHash(client.passwordAlgorithm);
-  hash.update(pass);
-  hash.update(client.passwordSalt);
-  if (hash.digest('hex') === client.passwordHash) {
-    return callback(null, true);
-  } else {
-    return callback(null, false);
-  }
-};
-
- function comparePasswordWithClientHash(pass, client, callback) {
-  let needle;
-  if ((needle = client.passwordAlgorithm, Array.from(crypto.getHashes()).includes(needle))) {
-    return cryptoCompare(pass, client, callback);
-  } else {
-    return bcryptCompare(pass, client, callback);
-  }
-};
+function comparePasswordWithClientHash(pass, client, callback) {
+	let needle;
+	if (Array.from(crypto.getHashes()).includes(client.passwordAlgorithm)) {
+		return cryptoCompare(pass, client, callback);
+	} else {
+		return bcryptCompare(pass, client, callback);
+	}
+}
 
 
 export function authenticateUser(ctx, done) {
-  let user = auth(ctx);
+	const user = auth(ctx);
 
-  if (user) {
-    return Client.findOne({ clientID: user.name }, function(err, client) {
-      if (err) { return done(err); }
+	if (user) {
+		return Client.findOne({ clientID: user.name }, (err, client) => {
+			if (err) { return done(err); }
 
-      if (client) {
-        if (!(client.passwordAlgorithm && client.passwordHash)) {
-          logger.warn(`${user.name} does not have a basic auth password set`);
-          return done(null, null);
-        }
+			if (client) {
+				if (!(client.passwordAlgorithm && client.passwordHash)) {
+					logger.warn(`${user.name} does not have a basic auth password set`);
+					return done(null, null);
+				}
 
-        return comparePasswordWithClientHash(user.pass, client, function(err, res) {
-          if (err) { return done(err); }
+				return comparePasswordWithClientHash(user.pass, client, (err, res) => {
+					if (err) { return done(err); }
 
-          if (res) {
-            logger.info(`${user.name} is authenticated.`);
-            ctx.authenticated = client;
-            ctx.authenticationType = 'basic';
-            return done(null, client);
-          } else {
-            logger.info(`${user.name} could NOT be authenticated, trying next auth mechanism if any...`);
-            return done(null, null);
-          }
-        });
-      } else {
-        logger.info(`${user.name} not found, trying next auth mechanism if any...`);
-        return done(null, null);
-      }
-    });
-  } else {
-    logger.info("No basic auth details supplied, trying next auth mechanism if any...");
-    ctx.authenticated = null; // Set to empty object rather than null
-    return done(null, null);
-  }
+					if (res) {
+						logger.info(`${user.name} is authenticated.`);
+						ctx.authenticated = client;
+						ctx.authenticationType = "basic";
+						return done(null, client);
+					} else {
+						logger.info(`${user.name} could NOT be authenticated, trying next auth mechanism if any...`);
+						return done(null, null);
+					}
+				});
+			} else {
+				logger.info(`${user.name} not found, trying next auth mechanism if any...`);
+				return done(null, null);
+			}
+		});
+	} else {
+		logger.info("No basic auth details supplied, trying next auth mechanism if any...");
+		ctx.authenticated = null; // Set to empty object rather than null
+		return done(null, null);
+	}
 }
 
 /*
  * Koa middleware for authentication by basic auth
  */
-export function koaMiddleware(next) {
-  let startTime;
-  if (statsdServer.enabled) { startTime = new Date(); }
-  if (this.authenticated != null) {
-    return {}; //TODO:Fix yield next
-  } else {
-    let authenticateUser = Q.denodeify(exports.authenticateUser);
-    ({}); //TODO:Fix yield authenticateUser this
-    if ((this.authenticated != null ? this.authenticated.clientID : undefined) != null) {
-      this.header['X-OpenHIM-ClientID'] = this.authenticated.clientID;
-    }
-    if (statsdServer.enabled) { sdc.timing(`${domain}.basicAuthMiddleware`, startTime); }
-    return {}; //TODO:Fix yield next
-  }
+export function* koaMiddleware(next) {
+	let startTime;
+	if (statsdServer.enabled) { startTime = new Date(); }
+	if (this.authenticated != null) {
+		return yield next;
+	} else {
+		const authenticateUser = Q.denodeify(exports.authenticateUser);
+		yield authenticateUser(this);
+		if ((this.authenticated != null ? this.authenticated.clientID : undefined) != null) {
+			this.header["X-OpenHIM-ClientID"] = this.authenticated.clientID;
+		}
+		if (statsdServer.enabled) { sdc.timing(`${domain}.basicAuthMiddleware`, startTime); }
+		return yield next;
+	}
 }
