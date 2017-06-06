9869acdaa67719fb3ff5f4faa8831bd4093c61a3
Andrew Ross <andrew.ross@jembi.org>
2017-06-06 10:15:10 +0200

    Completed authentication and authorization

--- src/api/authorisation.js
+++ src/api/authorisation.js
@@ -1,39 +1,35 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-import { Channel } from "../model/channels";
-import logger from 'winston';
+import logger from "winston";
 import Q from "q";
+import { Channel } from "../model/channels";
 
 export function inGroup(group, user) {
-  return user.groups.indexOf(group) >= 0;
+	return user.groups.indexOf(group) >= 0;
 }
 
-//#
+// #
 // A promise returning function that returns the list
 // of viewable channels for a user.
-//#
+// #
 export function getUserViewableChannels(user) {
-
-  // if admin allow all channel
-  if (exports.inGroup('admin', user)) {
-    return Channel.find({}).exec();
-  } else {
-    // otherwise figure out what this user can view
-    return Channel.find({ txViewAcl: { $in: user.groups } }).exec();
-  }
+	// if admin allow all channel
+	if (inGroup("admin", user)) {
+		return Channel.find({}).exec();
+	} else {
+		// otherwise figure out what this user can view
+		return Channel.find({ txViewAcl: { $in: user.groups } }).exec();
+	}
 }
 
-//#
+// #
 // A promise returning function that returns the list
 // of rerunnable channels for a user.
-//#
+// #
 export function getUserRerunableChannels(user) {
-
-  // if admin allow all channel
-  if (exports.inGroup('admin', user)) {
-    return Channel.find({}).exec();
-  } else {
-    // otherwise figure out what this user can rerun
-    return Channel.find({ txRerunAcl: { $in: user.groups } }).exec();
-  }
+	// if admin allow all channel
+	if (inGroup("admin", user)) {
+		return Channel.find({}).exec();
+	} else {
+		// otherwise figure out what this user can rerun
+		return Channel.find({ txRerunAcl: { $in: user.groups } }).exec();
+	}
 }
