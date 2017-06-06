ba7c8d8732e2beaaee53dc18167cdc0f1e891e14
Andrew Ross <andrew.ross@jembi.org>
2017-06-06 09:24:22 +0200

    Converted about.js

--- src/api/about.js
+++ src/api/about.js
@@ -1,18 +1,14 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-import { version as currentCoreVersion } from '../../package.json';
+import logger from "winston";
+import utils from "../utils";
+import { version as currentCoreVersion } from "../../package.json";
 
-import logger from 'winston';
-import utils from '../utils';
-
-export function getAboutInformation() {
-  
-  try {
-    this.body = {}; //TODO:Fix yield { currentCoreVersion: currentCoreVersion, serverTimezone: utils.serverTimezone() }
-    this.status = 200;
-    return logger.info(`User ${this.authenticated.email} successfully fetched 'about' information`);
-  } catch (e) {
-    this.body = e.message;
-    return utils.logAndSetResponse(this, 500, `Could not fetch 'about' info via the API ${e}`, 'error');
-  }
+export function* getAboutInformation() {
+	try {
+		this.body = yield { currentCoreVersion, serverTimezone: utils.serverTimezone() };
+		this.status = 200;
+		return logger.info(`User ${this.authenticated.email} successfully fetched 'about' information`);
+	} catch (e) {
+		this.body = e.message;
+		return utils.logAndSetResponse(this, 500, `Could not fetch 'about' info via the API ${e}`, "error");
+	}
 }
