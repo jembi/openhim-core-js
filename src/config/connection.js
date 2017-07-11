7600add7581cd348a8562643bdf0cd4fa93fbe1f
Tresor <tresor.mvumbi@jembi.org>
2017-07-11 14:18:37 +0200

    OHM-450 Add options to mongoose.createConnection
    Note: code not working yet

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 import { config } from "./";
+import * as utils from "../utils";
 
 config.mongo = config.get("mongo");
 
-export const connectionDefault = mongoose.createConnection(config.mongo.url);
+console.log('======================================================================');
+console.log(utils.getExtraMongoConfig());
+console.log('======================================================================');
+export const connectionDefault = mongoose.createConnection(config.mongo.url, utils.getExtraMongoConfig());
 export const connectionATNA = mongoose.createConnection(config.mongo.atnaUrl);
