7c131deb53e6687b3970112bcc91f600889842bb
Martin Brocker <martin@jembi.org>
2017-07-13 13:47:23 +0200

    OHM-443: refactor load utils to generate correct Auth headers

--- test/load-tests/load-test-utils.js
+++ test/load-tests/load-test-utils.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const crypto = require('crypto')
+const request = require('request')
 
 module.exports = {
   setAuthHeaders: (requestParams, context, ee, next) => {
@@ -8,26 +9,40 @@ module.exports = {
       requestParams.headers = {}
     }
     
-    const getAuthHeaders = () => {
-      const username = 'root@openhim.org'
-      const authTS = new Date().toISOString()
-      const requestSalt = '842cd4a0-1a91-45a7-bf76-c292cb36b2e8'
-      const passwordHash = "4b6010ecb28c6cc97a7c617ec08a3a840e8bc845c09201d8d3cc2a0d66e90e605f5b02c214488bff678a2f6b13f9863c7949bbacf4ee8a04e0b7aff8088b7cdd"
-      
-      const tokenhash = crypto.createHash('sha512')
-      tokenhash.update(passwordHash)
-      tokenhash.update(requestSalt)
-      tokenhash.update(authTS)
-      
-      return {
-        'auth-username': username,
-        'auth-ts': authTS,
-        'auth-salt': requestSalt,
-        'auth-token': tokenhash.digest('hex')
+    const getAuthHeaders = (username, password, callback) => {
+      const options = {
+        url: `https://localhost:8080/authenticate/${username}`,
+        json: true,
+        rejectUnauthorized: false
       }
+
+      request(options, (err, res, body) => {
+        if (err) {
+          return callback(err)
+        }
+
+        const passhash = crypto.createHash('sha512')
+        passhash.update(body.salt)
+        passhash.update(password)
+        const tokenhash = crypto.createHash('sha512')
+        tokenhash.update(passhash.digest('hex'))
+        tokenhash.update(body.salt)
+        tokenhash.update(body.ts)
+
+        const auth = {
+          'auth-username': username,
+          'auth-ts': body.ts,
+          'auth-salt': body.salt,
+          'auth-token': tokenhash.digest('hex')
+        }
+
+        callback(null, auth)
+      })
     }
     
-    Object.assign(requestParams.headers, getAuthHeaders())
-    next()
+    getAuthHeaders('root@openhim.org', 'openhim-password', (err, headers) => {
+      Object.assign(requestParams.headers, headers)
+      next()  
+    })    
   },
 }
