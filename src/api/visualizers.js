e46c59df074fcab6f7e495144d67d02b2f93cced
Brett Ausmeier <brett@jembi.org>
2017-09-07 16:28:17 +0200

    Fix visualiser endpoints
    To work with the latest version of Koa.
    
    OHM-467

--- src/api/visualizers.js
+++ src/api/visualizers.js
@@ -5,75 +5,75 @@ import * as authorisation from './authorisation'
 import * as utils from '../utils'
 
 // Endpoint that returns all visualizers
-export function * getVisualizers () {
+export async function getVisualizers (ctx) {
   // Must be admin
-  if (!authorisation.inGroup('admin', this.authenticated)) {
-    return utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to getVisualizers denied.`, 'info')
+  if (!authorisation.inGroup('admin', ctx.authenticated)) {
+    return utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to getVisualizers denied.`, 'info')
   }
 
   try {
-    this.body = yield VisualizerModelAPI.find().exec()
+    ctx.body = await VisualizerModelAPI.find().exec()
   } catch (err) {
-    return utils.logAndSetResponse(this, 500, `Could not fetch visualizers via the API: ${err}`, 'error')
+    utils.logAndSetResponse(ctx, 500, `Could not fetch visualizers via the API: ${err}`, 'error')
   }
 }
 
 // Endpoint that returns specific visualizer by visualizerId
-export function * getVisualizer (visualizerId) {
+export async function getVisualizer (ctx, visualizerId) {
   // Must be admin
-  if (!authorisation.inGroup('admin', this.authenticated)) {
-    return utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to getVisualizer denied.`, 'info')
+  if (!authorisation.inGroup('admin', ctx.authenticated)) {
+    return utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to getVisualizer denied.`, 'info')
   }
 
   visualizerId = unescape(visualizerId)
 
   try {
-    const result = yield VisualizerModelAPI.findById(visualizerId).exec()
+    const result = await VisualizerModelAPI.findById(visualizerId).exec()
     if (!result) {
-      this.body = `Visualizer with _id ${visualizerId} could not be found.`
-      this.status = 404
+      ctx.body = `Visualizer with _id ${visualizerId} could not be found.`
+      ctx.status = 404
     } else {
-      this.body = result
+      ctx.body = result
     }
   } catch (err) {
-    return utils.logAndSetResponse(this, 500, `Could not fetch visualizer via the API: ${err}`, 'error')
+    utils.logAndSetResponse(ctx, 500, `Could not fetch visualizer via the API: ${err}`, 'error')
   }
 }
 
 // Endpoint to add new visualizer
-export function * addVisualizer () {
+export async function addVisualizer (ctx) {
   // Must be admin user
-  if (!authorisation.inGroup('admin', this.authenticated)) {
-    return utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to addVisualizer denied.`, 'info')
+  if (!authorisation.inGroup('admin', ctx.authenticated)) {
+    return utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to addVisualizer denied.`, 'info')
   }
 
-  const visualizerData = this.request.body
+  const visualizerData = ctx.request.body
   if (!visualizerData) {
-    return utils.logAndSetResponse(this, 404, 'Cannot Add Visualizer, no request object', 'info')
+    return utils.logAndSetResponse(ctx, 404, 'Cannot Add Visualizer, no request object', 'info')
   }
 
   try {
     const visualizer = new VisualizerModelAPI(visualizerData)
-    yield Q.ninvoke(visualizer, 'save')
+    await Q.ninvoke(visualizer, 'save')
 
-    this.body = 'Visualizer successfully created'
-    this.status = 201
-    return logger.info('User %s created visualizer with id %s', this.authenticated.email, visualizer.id)
+    ctx.body = 'Visualizer successfully created'
+    ctx.status = 201
+    logger.info('User %s created visualizer with id %s', ctx.authenticated.email, visualizer.id)
   } catch (err) {
-    return utils.logAndSetResponse(this, 500, `Could not add visualizer via the API: ${err}`, 'error')
+    utils.logAndSetResponse(ctx, 500, `Could not add visualizer via the API: ${err}`, 'error')
   }
 }
 
 // Endpoint to update specific visualizer by visualizerId
-export function * updateVisualizer (visualizerId) {
+export async function updateVisualizer (ctx, visualizerId) {
   // Must be admin
-  if (!authorisation.inGroup('admin', this.authenticated)) {
-    return utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to updateVisualizer denied.`, 'info')
+  if (!authorisation.inGroup('admin', ctx.authenticated)) {
+    return utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to updateVisualizer denied.`, 'info')
   }
 
-  const visualizerData = this.request.body
+  const visualizerData = ctx.request.body
   if (!visualizerData) {
-    return utils.logAndSetResponse(this, 404, `Cannot Update Visualizer with _id ${visualizerId}, no request object`, 'info')
+    return utils.logAndSetResponse(ctx, 404, `Cannot Update Visualizer with _id ${visualizerId}, no request object`, 'info')
   }
 
   visualizerId = unescape(visualizerId)
@@ -82,36 +82,36 @@ export function * updateVisualizer (visualizerId) {
   if (visualizerData._id) { delete visualizerData._id }
 
   try {
-    const result = yield VisualizerModelAPI.findByIdAndUpdate(visualizerId, visualizerData).exec()
+    const result = await VisualizerModelAPI.findByIdAndUpdate(visualizerId, visualizerData).exec()
     if (!result) {
-      return utils.logAndSetResponse(this, 404, `Cannot Update Visualizer with _id ${visualizerId}, does not exist`, 'info')
+      return utils.logAndSetResponse(ctx, 404, `Cannot Update Visualizer with _id ${visualizerId}, does not exist`, 'info')
     }
 
-    this.body = `Successfully updated visualizer with _id ${visualizerId}`
-    return logger.info(`User ${this.authenticated.email} updated visualizer with _id ${visualizerId}`)
+    ctx.body = `Successfully updated visualizer with _id ${visualizerId}`
+    logger.info(`User ${ctx.authenticated.email} updated visualizer with _id ${visualizerId}`)
   } catch (e) {
-    return utils.logAndSetResponse(this, 500, `Could not update visualizer with _id ${visualizerId} via the API ${e}`, 'error')
+    utils.logAndSetResponse(ctx, 500, `Could not update visualizer with _id ${visualizerId} via the API ${e}`, 'error')
   }
 }
 
 // Endpoint to remove specific visualizer by visualizerId
-export function * removeVisualizer (visualizerId) {
+export async function removeVisualizer (ctx, visualizerId) {
   // Must be admin
-  if (!authorisation.inGroup('admin', this.authenticated)) {
-    return utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to removeVisualizer denied.`, 'info')
+  if (!authorisation.inGroup('admin', ctx.authenticated)) {
+    return utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to removeVisualizer denied.`, 'info')
   }
 
   visualizerId = unescape(visualizerId)
 
   try {
-    const v = yield VisualizerModelAPI.findByIdAndRemove(visualizerId).exec()
+    const v = await VisualizerModelAPI.findByIdAndRemove(visualizerId).exec()
     if (!v) {
-      return utils.logAndSetResponse(this, 404, `Could not find visualizer with _id ${visualizerId}`, 'info')
+      return utils.logAndSetResponse(ctx, 404, `Could not find visualizer with _id ${visualizerId}`, 'info')
     }
 
-    this.body = `Successfully removed visualizer with _id ${visualizerId}`
-    return logger.info(`User ${this.authenticated.email} removed visualizer with _id ${visualizerId}`)
+    ctx.body = `Successfully removed visualizer with _id ${visualizerId}`
+    logger.info(`User ${ctx.authenticated.email} removed visualizer with _id ${visualizerId}`)
   } catch (e) {
-    return utils.logAndSetResponse(this, 500, `Could not remove visualizer with _id ${visualizerId} via the API ${e}`, 'error')
+    utils.logAndSetResponse(ctx, 500, `Could not remove visualizer with _id ${visualizerId} via the API ${e}`, 'error')
   }
 }
