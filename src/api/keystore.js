42af869ed8e2983f6ad882d221566c7687a6c48b
Brett Ausmeier <brett@jembi.org>
2017-09-07 15:00:34 +0200

    Fix certificate related endpoints
    To work with the latest version of Koa.
    
    OHM-467

--- src/api/keystore.js
+++ src/api/keystore.js
@@ -7,143 +7,143 @@ import { config } from '../config'
 
 config.certificateManagement = config.get('certificateManagement')
 
-export function * getServerCert () {
+export async function getServerCert (ctx) {
   // Must be admin
-  if (authorisation.inGroup('admin', this.authenticated) === false) {
-    utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to getServerCert denied.`, 'info')
+  if (authorisation.inGroup('admin', ctx.authenticated) === false) {
+    utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to getServerCert denied.`, 'info')
     return
   }
 
   try {
-    const keystoreDoc = yield KeystoreModelAPI.findOne().lean('cert').exec()
+    const keystoreDoc = await KeystoreModelAPI.findOne().lean('cert').exec()
     keystoreDoc.cert.watchFSForCert = config.certificateManagement.watchFSForCert
-    this.body = keystoreDoc.cert
+    ctx.body = keystoreDoc.cert
   } catch (err) {
-    return utils.logAndSetResponse(this, 500, `Could not fetch the server cert via the API: ${err}`, 'error')
+    utils.logAndSetResponse(ctx, 500, `Could not fetch the server cert via the API: ${err}`, 'error')
   }
 }
 
-export function * getCACerts () {
+export async function getCACerts (ctx) {
   // Must be admin
-  if (authorisation.inGroup('admin', this.authenticated) === false) {
-    utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to getCACerts denied.`, 'info')
+  if (authorisation.inGroup('admin', ctx.authenticated) === false) {
+    utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to getCACerts denied.`, 'info')
     return
   }
 
   try {
-    const keystoreDoc = yield KeystoreModelAPI.findOne().select('ca').exec()
-    this.body = keystoreDoc.ca
+    const keystoreDoc = await KeystoreModelAPI.findOne().select('ca').exec()
+    ctx.body = keystoreDoc.ca
   } catch (err) {
-    return utils.logAndSetResponse(this, 500, `Could not fetch the ca certs trusted by this server via the API: ${err}`, 'error')
+    utils.logAndSetResponse(ctx, 500, `Could not fetch the ca certs trusted by this server via the API: ${err}`, 'error')
   }
 }
 
-export function * getCACert (certId) {
+export async function getCACert (ctx, certId) {
   // Must be admin
-  if (authorisation.inGroup('admin', this.authenticated) === false) {
-    utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to getCACert by id denied.`, 'info')
+  if (authorisation.inGroup('admin', ctx.authenticated) === false) {
+    utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to getCACert by id denied.`, 'info')
     return
   }
 
   try {
-    const keystoreDoc = yield KeystoreModelAPI.findOne().select('ca').exec()
+    const keystoreDoc = await KeystoreModelAPI.findOne().select('ca').exec()
     const cert = keystoreDoc.ca.id(certId)
 
-    this.body = cert
+    ctx.body = cert
   } catch (err) {
-    return utils.logAndSetResponse(this, 500, `Could not fetch ca cert by id via the API: ${err}`, 'error')
+    utils.logAndSetResponse(ctx, 500, `Could not fetch ca cert by id via the API: ${err}`, 'error')
   }
 }
 
-export function * setServerPassphrase () {
+export async function setServerPassphrase (ctx) {
   // Must be admin
-  if (authorisation.inGroup('admin', this.authenticated) === false) {
-    utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to setServerPassphrase denied.`, 'info')
+  if (authorisation.inGroup('admin', ctx.authenticated) === false) {
+    utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to setServerPassphrase denied.`, 'info')
     return
   }
 
   try {
-    const {passphrase} = this.request.body
-    const keystoreDoc = yield KeystoreModelAPI.findOne().exec()
+    const {passphrase} = ctx.request.body
+    const keystoreDoc = await KeystoreModelAPI.findOne().exec()
     keystoreDoc.passphrase = passphrase
-    yield Q.ninvoke(keystoreDoc, 'save')
-    this.status = 201
+    await Q.ninvoke(keystoreDoc, 'save')
+    ctx.status = 201
   } catch (err) {
-    return utils.logAndSetResponse(this, 500, `Could not set the passphrase  via the API: ${err}`, 'error')
+    utils.logAndSetResponse(ctx, 500, `Could not set the passphrase  via the API: ${err}`, 'error')
   }
 }
 
-export function * setServerCert () {
+export async function setServerCert (ctx) {
   // Must be admin
   let err
-  if (authorisation.inGroup('admin', this.authenticated) === false) {
-    utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to setServerCert by id denied.`, 'info')
+  if (authorisation.inGroup('admin', ctx.authenticated) === false) {
+    utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to setServerCert by id denied.`, 'info')
     return
   }
 
   if (config.certificateManagement.watchFSForCert) {
-    utils.logAndSetResponse(this, 400, 'Failed to upload server certificate: Uploading of certificate while watchFSForCert is true is not allowed.', 'info')
+    utils.logAndSetResponse(ctx, 400, 'Failed to upload server certificate: Uploading of certificate while watchFSForCert is true is not allowed.', 'info')
     return
   }
 
   try {
     let certInfo
     let fingerprint
-    const {cert, passphrase} = this.request.body
+    const {cert, passphrase} = ctx.request.body
     const readCertificateInfo = Q.denodeify(pem.readCertificateInfo)
     const getFingerprint = Q.denodeify(pem.getFingerprint)
     try {
-      certInfo = yield readCertificateInfo(cert)
-      fingerprint = yield getFingerprint(cert)
+      certInfo = await readCertificateInfo(cert)
+      fingerprint = await getFingerprint(cert)
     } catch (error) {
       err = error
-      return utils.logAndSetResponse(this, 400, `Could not add server cert via the API: ${err}`, 'error')
+      return utils.logAndSetResponse(ctx, 400, `Could not add server cert via the API: ${err}`, 'error')
     }
     certInfo.data = cert
     certInfo.fingerprint = fingerprint.fingerprint
 
-    const keystoreDoc = yield KeystoreModelAPI.findOne().exec()
+    const keystoreDoc = await KeystoreModelAPI.findOne().exec()
     keystoreDoc.cert = certInfo
     keystoreDoc.passphrase = passphrase
 
-    yield Q.ninvoke(keystoreDoc, 'save')
-    this.status = 201
+    await Q.ninvoke(keystoreDoc, 'save')
+    ctx.status = 201
   } catch (error1) {
     err = error1
-    return utils.logAndSetResponse(this, 500, `Could not add server cert via the API: ${err}`, 'error')
+    utils.logAndSetResponse(ctx, 500, `Could not add server cert via the API: ${err}`, 'error')
   }
 }
 
-export function * setServerKey () {
+export async function setServerKey (ctx) {
   // Must be admin
-  if (authorisation.inGroup('admin', this.authenticated) === false) {
-    utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to getServerKey by id denied.`, 'info')
+  if (authorisation.inGroup('admin', ctx.authenticated) === false) {
+    utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to getServerKey by id denied.`, 'info')
     return
   }
 
   try {
-    const {key, passphrase} = this.request.body
-    const keystoreDoc = yield KeystoreModelAPI.findOne().exec()
+    const {key, passphrase} = ctx.request.body
+    const keystoreDoc = await KeystoreModelAPI.findOne().exec()
     keystoreDoc.key = key
     keystoreDoc.passphrase = passphrase
-    yield Q.ninvoke(keystoreDoc, 'save')
-    this.status = 201
+    await Q.ninvoke(keystoreDoc, 'save')
+    ctx.status = 201
   } catch (err) {
-    return utils.logAndSetResponse(this, 500, `Could not add server key via the API: ${err}`, 'error')
+    return utils.logAndSetResponse(ctx, 500, `Could not add server key via the API: ${err}`, 'error')
   }
 }
 
-export function * addTrustedCert () {
+export async function addTrustedCert (ctx) {
   // Must be admin
   let err
-  if (authorisation.inGroup('admin', this.authenticated) === false) {
-    utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to addTrustedCert by id denied.`, 'info')
+  if (authorisation.inGroup('admin', ctx.authenticated) === false) {
+    utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to addTrustedCert by id denied.`, 'info')
     return
   }
 
   try {
     let invalidCert = false
-    let chain = this.request.body.cert
+    let chain = ctx.request.body.cert
 
     // Parse into an array in case this is a cert chain
     // (code derived from: http://www.benjiegillam.com/2012/06/node-dot-js-ssl-certificate-chain/)
@@ -160,7 +160,7 @@ export function * addTrustedCert () {
       }
     }
 
-    const keystoreDoc = yield KeystoreModelAPI.findOne().exec()
+    const keystoreDoc = await KeystoreModelAPI.findOne().exec()
     const readCertificateInfo = Q.denodeify(pem.readCertificateInfo)
     const getFingerprint = Q.denodeify(pem.getFingerprint)
 
@@ -172,8 +172,8 @@ export function * addTrustedCert () {
       let certInfo
       let fingerprint
       try {
-        certInfo = yield readCertificateInfo(cert)
-        fingerprint = yield getFingerprint(cert)
+        certInfo = await readCertificateInfo(cert)
+        fingerprint = await getFingerprint(cert)
       } catch (error) {
         err = error
         invalidCert = true
@@ -184,59 +184,55 @@ export function * addTrustedCert () {
       keystoreDoc.ca.push(certInfo)
     }
 
-    yield Q.ninvoke(keystoreDoc, 'save')
+    await Q.ninvoke(keystoreDoc, 'save')
 
     if (invalidCert) {
-      return utils.logAndSetResponse(this, 400, `Failed to add one more cert, are they valid? ${err}`, 'error')
+      utils.logAndSetResponse(ctx, 400, `Failed to add one more cert, are they valid? ${err}`, 'error')
     } else {
-      this.status = 201
+      ctx.status = 201
     }
   } catch (error1) {
     err = error1
-    return utils.logAndSetResponse(this, 500, `Could not add trusted cert via the API: ${err}`, 'error')
+    utils.logAndSetResponse(ctx, 500, `Could not add trusted cert via the API: ${err}`, 'error')
   }
 }
 
-export function * removeCACert (certId) {
+export async function removeCACert (ctx, certId) {
   // Must be admin
-  if (authorisation.inGroup('admin', this.authenticated) === false) {
-    utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to removeCACert by id denied.`, 'info')
+  if (authorisation.inGroup('admin', ctx.authenticated) === false) {
+    utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to removeCACert by id denied.`, 'info')
     return
   }
 
   try {
-    const keystoreDoc = yield KeystoreModelAPI.findOne().exec()
+    const keystoreDoc = await KeystoreModelAPI.findOne().exec()
     keystoreDoc.ca.id(certId).remove()
-    yield Q.ninvoke(keystoreDoc, 'save')
-    this.status = 200
+    await Q.ninvoke(keystoreDoc, 'save')
+    ctx.status = 200
   } catch (err) {
-    return utils.logAndSetResponse(this, 500, `Could not remove ca cert by id via the API: ${err}`, 'error')
+    utils.logAndSetResponse(ctx, 500, `Could not remove ca cert by id via the API: ${err}`, 'error')
   }
 }
 
-export function * verifyServerKeys () {
+export async function verifyServerKeys (ctx) {
   // Must be admin
-  let err
-  if (authorisation.inGroup('admin', this.authenticated) === false) {
-    utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to verifyServerKeys.`, 'info')
+  if (authorisation.inGroup('admin', ctx.authenticated) === false) {
+    utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to verifyServerKeys.`, 'info')
     return
   }
 
   try {
     let result
     try {
-      result = yield Q.nfcall(getCertKeyStatus)
+      result = await Q.nfcall(getCertKeyStatus)
     } catch (error) {
-      err = error
-      return utils.logAndSetResponse(this, 400, `Could not verify certificate and key, are they valid? ${err}`, 'error')
+      return utils.logAndSetResponse(ctx, 400, `Could not verify certificate and key, are they valid? ${error}`, 'error')
     }
 
-    this.body =
-      {valid: result}
-    this.status = 200
-  } catch (error1) {
-    err = error1
-    return utils.logAndSetResponse(this, 500, `Could not determine validity via the API: ${err}`, 'error')
+    ctx.body = {valid: result}
+    ctx.status = 200
+  } catch (error) {
+    utils.logAndSetResponse(ctx, 500, `Could not determine validity via the API: ${error}`, 'error')
   }
 }
 
--- src/api/restart.js
+++ src/api/restart.js
@@ -15,32 +15,32 @@ config.tcpAdapter = config.get('tcpAdapter')
 /*
  * restart the server
  */
-export function * restart (next) {
+export async function restart (ctx, next) {
   // Test if the user is authorised
-  if (authorisation.inGroup('admin', this.authenticated) === false) {
-    utils.logAndSetResponse(this, 403, `User ${this.authenticated.email} is not an admin, API access to restart the server denied.`, 'info')
+  if (authorisation.inGroup('admin', ctx.authenticated) === false) {
+    utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to restart the server denied.`, 'info')
     return
   }
 
   try {
-    const emailAddr = this.authenticated.email
+    const emailAddr = ctx.authenticated.email
 
-    const result = yield Q.nfcall(KeystoreAPI.getCertKeyStatus)
+    const result = await Q.nfcall(KeystoreAPI.getCertKeyStatus)
 
     // valid certificate/key
     if (result) {
       server.startRestartServerTimeout(() => logger.info(`User ${emailAddr} has requested a Server Restart. Proceeding to restart servers...`))
 
       // All ok! So set the result
-      this.body = 'Server being restarted'
-      this.status = 200
+      ctx.body = 'Server being restarted'
+      ctx.status = 200
     } else {
       // Not valid
       logger.info(`User ${emailAddr} has requested a Server Restart with invalid certificate details. Cancelling restart...`)
-      this.body = 'Certificates and Key did not match. Cancelling restart...'
-      this.status = 400
+      ctx.body = 'Certificates and Key did not match. Cancelling restart...'
+      ctx.status = 400
     }
   } catch (e) {
-    return utils.logAndSetResponse(this, 400, `Could not restart the servers via the API: ${e}`, 'error')
+    utils.logAndSetResponse(ctx, 400, `Could not restart the servers via the API: ${e}`, 'error')
   }
 }
