aeabce8520be11cef4a6070d317a1b85f3b4bb32
Nathan Floor <nathan.floor@jembi.org>
2017-10-06 13:10:46 +0200

    Updated travis config to build docker image and added permissions for build script

--- .travis/build_docker.sh
+++ .travis/build_docker.sh
@@ -1,3 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
-curl -H 'Content-Type: application/json' --data '{"source_type": "Branch", "source_name": "core"}' -X POST https://registry.hub.docker.com/u/jembi/openhim-core/trigger/5cd6f182-c523-409e-ae68-9ab5de1f2849/
+if ([ "$TRAVIS_BRANCH" == "master" ] ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "core"}' -X POST https://registry.hub.docker.com/u/jembi/openhim-core/trigger/5cd6f182-c523-409e-ae68-9ab5de1f2849/
+else
+    echo "Docker image will only be built for commits to master"
+fi
+
