cc9101f7c7adb0b05fae6646be8973742710c9ce
Nathan Floor <nathan.floor@jembi.org>
2017-10-10 08:30:09 +0200

    ohm-507: added mongo service to travis config

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -3,7 +3,6 @@ set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_78888cd548b2_key -iv $encrypted_78888cd548b2_iv -in .travis/deploy_key.enc -out deploy_key -d
-rm deploy_key.enc # Don't need it anymore
 chmod 600 deploy_key
 mv deploy_key ~/.ssh/id_rsa
 
