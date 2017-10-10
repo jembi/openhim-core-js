bd20a5d21968cbd1a6b82818ad20b2544b0c36e4
Nathan Floor <nathan.floor@jembi.org>
2017-10-10 09:03:07 +0200

    ohm-507: updated deploy script to include validation checks

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -x # Show the output of the following commands (useful for debugging)
 
+# Set variables
+REMOTE_TARGET=test
+if [[ $1 ]]; then
+    REMOTE_TARGET=$1 # target environment config: [test/staging]
+fi
+REMOTE_URL=188.166.147.164
+
 # Copy new Dockerfile to remote server
-if ! scp "travis_deploy@188.166.147.164:~/Dockerfile" resources/docker/Dockerfile; then
+if ! scp "travis_deploy@$REMOTE_URL:~/Dockerfile" resources/docker/Dockerfile; then
     echo 'Remote file did not exist.'
-    # scp resources/docker/Dockerfile travis_deploy@188.166.147.164:~
+    scp resources/docker/Dockerfile travis_deploy@$REMOTE_URL:~
 fi
 
 # Log into remote server
-ssh travis_deploy@188.166.147.164
+ssh travis_deploy@$REMOTE_URL
 
 # backup & shutown current containers
 docker ps
@@ -17,7 +24,9 @@ docker stop openhim-core-$REMOTE_TARGET
 docker rename openhim-core-$REMOTE_TARGET openhim-core-$REMOTE_TARGET-backup
 
 # Build docker image with latest changes
-docker build -t openhim-core-js resources/docker
+docker build -t jembi/openhim-core resources/docker
+rm Dockerfile # no-longer needed
 
 # install new container
-# docker run -d -p 8080:8080 -p 5000:5000 -p 5001:5001 --network=$REMOTE_TARGET --name=openhim-core-$REMOTE_TARGET openhim-core-js
+# docker run -d -p 8080:8080 -p 5000:5000 -p 5001:5001 --network=$REMOTE_TARGET --name=openhim-core-$REMOTE_TARGET jembi/openhim-core
+
