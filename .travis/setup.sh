fb86020d531fbef15d53ac65b61e1a7525f2a5ae
Nathan Floor <nathan.floor@jembi.org>
2017-10-10 08:02:54 +0200

    ohm-507: updated setup script

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -2,8 +2,8 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_78888cd548b2_key -iv $encrypted_78888cd548b2_iv -in deploy_key.enc -out deploy_key -d
-rm deploy-key.enc # Don't need it anymore
-chmod 600 deploy-key
-mv deploy-key ~/.ssh/id_rsa
+openssl aes-256-cbc -K $encrypted_78888cd548b2_key -iv $encrypted_78888cd548b2_iv -in .travis/deploy_key.enc -out deploy_key -d
+rm deploy_key.enc # Don't need it anymore
+chmod 600 deploy_key
+mv deploy_key ~/.ssh/id_rsa
 
