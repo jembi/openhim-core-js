4b2b603a27bd4ebfd870e52aeb9e93a75d96ced3
Matt <matthew.dickie@jembi.org>
2017-10-18 10:15:20 +0200

    Fix authentication api integration tests
    Mocha upgrade
    
    OHM-499

--- test/integration/authenticationAPITests.js
+++ test/integration/authenticationAPITests.js
@@ -35,7 +35,7 @@ describe('API Integration Tests', () =>
       ])
     })
 
-    it.only('should audit a successful login on an API endpoint', async () => {
+    it('should audit a successful login on an API endpoint', async () => {
       await request(constants.BASE_URL)
         .get('/channels')
         .set('auth-username', testUtils.rootUser.email)
@@ -54,157 +54,83 @@ describe('API Integration Tests', () =>
       audits[0].activeParticipant[1].userID.should.be.equal('root@jembi.org')
     })
 
-    it('should audit an unsuccessful login on an API endpoint', done =>
-      request('https://localhost:8080')
+    it('should audit an unsuccessful login on an API endpoint', async () => {
+      await request(constants.BASE_URL)
         .get('/channels')
         .set('auth-username', 'wrong@email.org')
         .set('auth-ts', authDetails.authTS)
         .set('auth-salt', authDetails.authSalt)
         .set('auth-token', authDetails.authToken)
         .expect(401)
-        .end((err, res) => {
-          if (err) {
-            return done(err)
-          } else {
-            const validateAudit = () =>
-              AuditModel.find({}, (err, audits) => {
-                if (err) { return done(err) }
-                if (audits.length > 1) {
-                  logger.error(JSON.stringify(audits, null, 2))
-                }
-                audits.length.should.be.exactly(1)
-                audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('8') // failure
-                audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
-                audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
-                audits[0].activeParticipant.length.should.be.exactly(2)
-                audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
-                audits[0].activeParticipant[1].userID.should.be.equal('wrong@email.org')
-                return done()
-              })
 
-            return setTimeout(validateAudit, 150 * global.testTimeoutFactor)
-          }
-        })
-    )
+      const audits = await AuditModel.find({})
+      audits.length.should.be.exactly(1)
+      audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('8') // failure
+      audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
+      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
+      audits[0].activeParticipant.length.should.be.exactly(2)
+      audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
+      audits[0].activeParticipant[1].userID.should.be.equal('wrong@email.org')
+    })
 
-    it('should NOT audit a successful login on an auditing exempt API endpoint', done =>
-      request('https://localhost:8080')
+    it('should NOT audit a successful login on an auditing exempt API endpoint', async () => {
+      await request(constants.BASE_URL)
         .get('/audits')
         .set('auth-username', testUtils.rootUser.email)
         .set('auth-ts', authDetails.authTS)
         .set('auth-salt', authDetails.authSalt)
         .set('auth-token', authDetails.authToken)
         .expect(200)
-        .end((err, res) => {
-          if (err) {
-            return done(err)
-          } else {
-            const validateAudit = () =>
-              AuditModel.find({}, (err, audits) => {
-                if (err) { return done(err) }
-                if (audits.length > 0) {
-                  logger.error(JSON.stringify(audits, null, 2))
-                }
-                audits.length.should.be.exactly(0)
-                return done()
-              })
+      const audits = await AuditModel.find({})
+      audits.length.should.be.exactly(0)
+    })
 
-            return setTimeout(validateAudit, 150 * global.testTimeoutFactor)
-          }
-        })
-    )
-
-    it('should audit an unsuccessful login on an auditing exempt API endpoint', done =>
-      request('https://localhost:8080')
+    it('should audit an unsuccessful login on an auditing exempt API endpoint', async () => {
+      await request(constants.BASE_URL)
         .get('/audits')
         .set('auth-username', 'wrong@email.org')
         .set('auth-ts', authDetails.authTS)
         .set('auth-salt', authDetails.authSalt)
         .set('auth-token', authDetails.authToken)
         .expect(401)
-        .end((err, res) => {
-          if (err) {
-            return done(err)
-          } else {
-            const validateAudit = () =>
-              AuditModel.find({}, (err, audits) => {
-                if (err) { return done(err) }
-                if (audits.length > 1) {
-                  logger.error(JSON.stringify(audits, null, 2))
-                }
-                audits.length.should.be.exactly(1)
-                audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('8') // failure
-                audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
-                audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
-                audits[0].activeParticipant.length.should.be.exactly(2)
-                audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
-                audits[0].activeParticipant[1].userID.should.be.equal('wrong@email.org')
-                return done()
-              })
+      const audits = await AuditModel.find({})
+      audits.length.should.be.exactly(1)
+      audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('8') // failure
+      audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
+      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
+      audits[0].activeParticipant.length.should.be.exactly(2)
+      audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
+      audits[0].activeParticipant[1].userID.should.be.equal('wrong@email.org')
+    })
 
-            return setTimeout(validateAudit, 150 * global.testTimeoutFactor)
-          }
-        })
-    )
-
-    it('should NOT audit a successful login on /transactions if the view is not full', done =>
-      request('https://localhost:8080')
+    it('should NOT audit a successful login on /transactions if the view is not full', async () => {
+      await request(constants.BASE_URL)
         .get('/transactions') // default is simple
         .set('auth-username', testUtils.rootUser.email)
         .set('auth-ts', authDetails.authTS)
         .set('auth-salt', authDetails.authSalt)
         .set('auth-token', authDetails.authToken)
         .expect(200)
-        .end((err, res) => {
-          if (err) {
-            return done(err)
-          } else {
-            const validateAudit = () =>
-              AuditModel.find({}, (err, audits) => {
-                if (err) { return done(err) }
-                if (audits.length > 0) {
-                  logger.error(JSON.stringify(audits, null, 2))
-                }
-                audits.length.should.be.exactly(0)
-                return done()
-              })
+      const audits = await AuditModel.find({})
+      audits.length.should.be.exactly(0)
+    })
 
-            return setTimeout(validateAudit, 150 * global.testTimeoutFactor)
-          }
-        })
-    )
-
-    return it('should audit a successful login on /transactions if the view is full', done =>
-      request('https://localhost:8080')
+    it.only('should audit a successful login on /transactions if the view is full', async () => {
+      await request(constants.BASE_URL)
         .get('/transactions?filterRepresentation=full')
         .set('auth-username', testUtils.rootUser.email)
         .set('auth-ts', authDetails.authTS)
         .set('auth-salt', authDetails.authSalt)
         .set('auth-token', authDetails.authToken)
         .expect(200)
-        .end((err, res) => {
-          if (err) {
-            return done(err)
-          } else {
-            const validateAudit = () =>
-              AuditModel.find({}, (err, audits) => {
-                if (err) { return done(err) }
-                if (audits.length > 1) {
-                  logger.error(JSON.stringify(audits, null, 2))
-                }
-                audits.length.should.be.exactly(1)
-                audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('0') // success
-                audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
-                audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
-                audits[0].activeParticipant.length.should.be.exactly(2)
-                audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
-                audits[0].activeParticipant[1].userID.should.be.equal('root@jembi.org')
-                return done()
-              })
-
-            return setTimeout(validateAudit, 150 * global.testTimeoutFactor)
-          }
-        })
-    )
+      const audits = await AuditModel.find({})
+      audits.length.should.be.exactly(1)
+      audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('0') // success
+      audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
+      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
+      audits[0].activeParticipant.length.should.be.exactly(2)
+      audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
+      audits[0].activeParticipant[1].userID.should.be.equal('root@jembi.org')
+    })
   })
 )
