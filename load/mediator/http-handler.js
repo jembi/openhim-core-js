0ab817d3a8bb3c5c373378506a93e25cd958c336
Brett Ausmeier <brett@jembi.org>
2017-11-16 16:58:13 +0200

    Add endpoint which responds immediately
    For stress testing the OpenHIM.
    
    OHM-552

--- load/mediator/http-handler.js
+++ load/mediator/http-handler.js
@@ -1,8 +1,16 @@
 'use strict'
 
-function handleRequest (req, res) {
+function respondImmediately (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end('Hello world')
+}
+
+function handleRequest (req, res) {
+  if (req.url === '/immediate') {
+    return respondImmediately(req, res)
+  }
+  res.writeHead(404)
+  res.end()
 }
 
 exports.handleRequest = handleRequest
