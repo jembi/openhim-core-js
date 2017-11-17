09fcd6e7c7fc86b73ab632d6c1106bb5ba33c574
Brett Ausmeier <brett@jembi.org>
2017-11-17 10:55:40 +0200

    Set content length header for TCP body response
    So that the downstream service knows how much content to expect.
    
    OHM-556

--- load/mediator/tcp-handler.js
+++ load/mediator/tcp-handler.js
@@ -5,7 +5,8 @@ const BodyStream = require('./body-stream')
 exports.handleBodyRequest = (conn) => {
   const length = 1024 * 1024
   conn.write('HTTP/1.1 200 OK\n')
-  conn.write('Content-Type: text/plain\n')
+  conn.write('Content-Type: text/plain; charset=utf-8\n')
+  conn.write(`Content-Length: ${length}\n`)
   conn.write('\n')
   new BodyStream(length).pipe(conn)
 }
