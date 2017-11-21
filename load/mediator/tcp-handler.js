c41b42b5baff0e28c4225983b27e6e55e6357dd3
Brett Ausmeier <brett@jembi.org>
2017-11-21 10:32:53 +0200

    Fix HTTP over TCP
    Don't send response headers before the request headers have been
    received.

--- load/mediator/tcp-handler.js
+++ load/mediator/tcp-handler.js
@@ -5,27 +5,36 @@ const DELAY = parseInt(process.env.DELAY, 10) || 2000
 const bigBuff = Buffer.alloc(2 * 1024 * 1024, 'HellWorld ')
 
 function sendHttpHeaders (conn) {
-  conn.write('HTTP/1.1 200 OK\n')
-  conn.write('Content-Type: text/plain\n')
-  conn.write('\n')
+  conn.write('HTTP/1.1 200 OK\r\n')
+  conn.write('Connection: close\r\n')
+  conn.write('Content-Encoding: identity\r\n')
+  conn.write('Content-Type: text/plain; charset=utf-8\r\n')
+  conn.write('\r\n')
 }
 
 exports.handleBodyRequest = (conn) => {
-  sendHttpHeaders(conn)
-  conn.end(bigBuff)
+  conn.on('error', console.error)
+  conn.once('data', () => {
+    sendHttpHeaders(conn)
+    conn.end(bigBuff)
+  })
 }
 
 exports.handleDelayRequest = (conn) => {
-  sendHttpHeaders(conn)
-  conn.write('Delay start')
-  setTimeout(() => {
-    conn.end(`Waited for ${DELAY}ms`)
-  }, DELAY)
-
-  conn.on('error', console.warn)
+  conn.on('error', console.error)
+  conn.once('data', () => {
+    sendHttpHeaders(conn)
+    conn.write('Delay start')
+    setTimeout(() => {
+      conn.end(`Waited for ${DELAY}ms`)
+    }, DELAY)
+  })
 }
 
 exports.handleImmediateRequest = (conn) => {
-  sendHttpHeaders(conn)
-  conn.end(`Immediate tcp response`)
+  conn.on('error', console.error)
+  conn.once('data', () => {
+    sendHttpHeaders(conn)
+    conn.end(`Immediate tcp response`)
+  })
 }
