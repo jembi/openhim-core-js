acb9122dbbb2183167795b9ef932e6158581f1a5
Brett Ausmeier <brett@jembi.org>
2017-11-28 10:06:27 +0200

    Ramp users from 0 -> 100 -> 0
    Test with multiple stages so that there's time for the OpenHIM to warm
    up and cool down instead of suddenly hammering it with peak load.
    
    OHM-571

--- load/load.js
+++ load/load.js
@@ -4,8 +4,11 @@ import {check} from 'k6'
 const BASE_URL = __ENV.BASE_URL || 'http://localhost:5001/http'
 
 export const options = {
-  duration: '30s',
-  vus: 100,
+  stages: [
+    {duration: '30s', target: 100},
+    {duration: '1m'},
+    {duration: '30s', target: 0}
+  ],
   thresholds: {
     http_req_duration: ['p(95)<600']
   }
