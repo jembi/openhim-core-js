2833b5a92462d978a9aa77bc303a7ca7d90ffaa5
Matt <matthew.dickie@jembi.org>
2017-12-01 13:02:05 +0200

    Change number of virtual users for test
    multiple users would interefer with each others requests
    
    OHM-574

--- performance/transactionsWithoutFilters.js
+++ performance/transactionsWithoutFilters.js
@@ -5,8 +5,8 @@ import {getTestAuthHeaders} from './auth.js'
 const BASE_URL = __ENV.BASE_URL || 'https://127.0.0.1:8080'
 
 export const options = {
-  vus: 10,
-  duration: '2m',
+  vus: 1,
+  iterations: 100,
   thresholds: {
     http_req_duration: ['p(95)<600']
   },
