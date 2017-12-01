6aae8b0f0ea973fec094c9973aade021a744b61c
tumijacob <matome.phoshoko@jembi.org>
2017-12-01 13:52:10 +0200

    OHM-547 Generate mock channels
    This commit simply adds a mock up channel for performance testing.

--- performance/seed.js
+++ performance/seed.js
@@ -3,21 +3,21 @@ const faker = require('faker')
 const { ClientModel, ChannelModel, TransactionModel } = require('../src/model')
 const { dropTestDb } = require('../test/utils')
 
-async function seedValues (clients = 1, channelsPerClient = 1, transactionsPerChannel = 1000, seed = 90123) {
+async function seedValues(clients = 1, channelsPerClient = 1, transactionsPerChannel = 1000, seed = 90123) {
   await dropTestDb()
   faker.seed(seed)
   for (let clientNum = 0; clientNum < clients; clientNum++) {
     const client = await createClient(clientNum)
     for (let channelNum = 0; channelNum < channelsPerClient; channelNum++) {
       const channel = await creatChannel(client, channelNum)
-      for(let transactionNum = 0; transactionNum < transactionsPerChannel; transactionNum++){
+      for (let transactionNum = 0; transactionNum < transactionsPerChannel; transactionNum++) {
         await createTransaction(channel, client, transactionNum)
       }
     }
   }
 }
 
-async function createClient (clientNum) {
+async function createClient(clientNum) {
   const contactPerson = {
     firstName: faker.name.firstName(),
     lastName: faker.name.lastName()
@@ -41,10 +41,28 @@ async function createClient (clientNum) {
   return client.save()
 }
 
-function creatChannel(client, channelNum){
+async function creatChannel(client, channelNum) {
+  const httpMethods = ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS', 'HEAD', 'TRACE', 'CONNECT', 'PATCH']
+  const routeDef = {
+    name: faker.name.findName(),
+    host: 'http//localhost:8080',
+    port: 3441,
+    primary: true,
+    type: 'http'
+  }
+
   const channel = new ChannelModel({
-    name :
+    name: `testChannel${channelNum}`,
+    urlPattern: '/encounters/.*$',
+    routes: routeDef,
+    type: 'http',
+    allow: [`${client.name}`],
+    methods: httpMethods,
+    addAutoRewriteRules: true,
+    __v: 1
   })
+
+  return channel.save()
 }
 
 if (module.parent == null) {
