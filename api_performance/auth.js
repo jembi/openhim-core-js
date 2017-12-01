43e5dee3224fe9877cda9ccd5dfb64d11a3a6423
Matt <matthew.dickie@jembi.org>
2017-12-01 08:20:36 +0200

    Add openhim authentication
    To authorise access to api
    
    OHM-574

---
+++ api_performance/auth.js
@@ -0,0 +1,18 @@
+import crypto from 'k6/crypto'
+
+const rootUser = {
+  email: 'root@openhim.org',
+  hash: 'a8cfb8a146612e0388decb184d1b14642cf6a77c0adf40c7bc37eb958364239af01e91297e76b9758de4b4abfc3018fc71bddc5d65cf2e9c1ca3929898d20514',
+  salt: 'b8fe0e678fff2bc83aa109cd6b28751f'
+}
+
+export function getTestAuthHeaders () {
+  const timestamp = new Date().toISOString()
+  return {
+    'Accept': 'application/json',
+    'auth-username': rootUser.email,
+    'auth-ts': timestamp,
+    'auth-salt': rootUser.salt,
+    'auth-token': crypto.sha512(rootUser.hash + rootUser.salt + timestamp, 'hex')
+  }
+}
