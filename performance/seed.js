9cd98dd9f0c3bdba2415ea5df6bb0de412e34cbf
Andrew Ross <andrew.ross@jembi.org>
2017-12-01 14:15:08 +0200

    Created semi random transactions

--- performance/seed.js
+++ performance/seed.js
@@ -3,15 +3,23 @@ const faker = require('faker')
 const { ClientModel, ChannelModel, TransactionModel } = require('../src/model')
 const { dropTestDb } = require('../test/utils')
 
-async function seedValues (clients = 1, channelsPerClient = 1, transactionsPerChannel = 1000, seed = 90123) {
+const DEFAULT_SEED = 9575
+
+const DEFAULT_START_DATE = new Date('2015/11/12')
+const DEFAULT_END_DATE = new Date('2018/11/12')
+
+async function seedValues (clients = 1, channelsPerClient = 1, transactionsPerChannel = 1000, startDate = DEFAULT_START_DATE, endDate = DEFAULT_END_DATE) {
   await dropTestDb()
-  faker.seed(seed)
+  faker.seed(DEFAULT_SEED)
+  const timeStep = Math.floor((endDate.getTime() - startDate.getTime()) / transactionsPerChannel)
+  // TODO : Make this a lot faster
   for (let clientNum = 0; clientNum < clients; clientNum++) {
     const client = await createClient(clientNum)
     for (let channelNum = 0; channelNum < channelsPerClient; channelNum++) {
       const channel = await creatChannel(client, channelNum)
-      for(let transactionNum = 0; transactionNum < transactionsPerChannel; transactionNum++){
-        await createTransaction(channel, client, transactionNum)
+      for (let transactionNum = 0; transactionNum < transactionsPerChannel; transactionNum++) {
+        const requestTime = new Date(startDate.getTime() + timeStep * transactionNum)
+        await createTransaction(channel, client, requestTime)
       }
     }
   }
@@ -41,10 +49,76 @@ async function createClient (clientNum) {
   return client.save()
 }
 
-function creatChannel(client, channelNum){
-  const channel = new ChannelModel({
-    name :
-  })
+function creatChannel (client, channelNum) {
+  // const channel = new ChannelModel({
+  //   name :
+  // })
+}
+
+function createTransaction (channel, client, requestTime) {
+  const request = {
+    host: faker.internet.ip(),
+    port: channel.port,
+    path: 'test/path',
+    headers: {
+      'Content-type': 'text/html'
+    },
+    method: oneOf(['POST', 'GET', 'PUT', 'DELETE']),
+    timestamp: new Date()
+  }
+
+  const transactionDoc = {
+    clientID: client._id,
+    clientIP: faker.internet.ip(),
+    channelID: channel._id,
+    request,
+    status: oneOf(['Processing', 'Failed', 'Completed', 'Successful', 'Completed with error(s)'])
+  }
+
+  if (transactionDoc.status !== 'Processing') {
+    const response = {
+      status: getStatusCode(transactionDoc.status),
+      headers: {
+        'Content-type': 'text/html'
+      },
+      timestamp: new Date()
+    }
+
+    if (response.status >= 500) {
+      response.error = {
+        message: 'Something failed'
+      }
+    } else {
+      response.body = getBody()
+    }
+
+    Object.assign(transactionDoc, { response })
+  }
+
+  return new TransactionModel(transactionDoc).save()
+}
+
+function getStatusCode (status) {
+  switch (status) {
+    case 'Failed': return 500
+    case 'Completed': return 400
+    case 'Successful': return 201
+    default: return 200
+  }
+}
+
+function getBody () {
+  switch (faker.random() % 6) {
+    case 0: return Buffer.alloc(100000, 'Large Response ').toString()
+    case 1:
+    case 2:
+    case 3: return `Response Body`
+    default: return ''
+  }
+}
+
+function oneOf (arr) {
+  return arr[faker.random.number() % arr.length]
 }
 
 if (module.parent == null) {
