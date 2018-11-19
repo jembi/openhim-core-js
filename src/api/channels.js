eb994ef210d09ccf44f6a613cac48aa98945e430
Brent Engelbrecht <brent.engelbrecht@jembi.org>
2018-11-19 12:33:32 +0200

    Fix brace -- no cake for you!

--- src/api/channels.js
+++ src/api/channels.js
@@ -217,7 +217,7 @@ export async function getChannel (ctx, channelId) {
   }
 }
 
-export async function getChannelAudits (ctx, channelId)
+export async function getChannelAudits (ctx, channelId) {
   if (!authorisation.inGroup('admin', ctx.authenticated)) {
     utils.logAndSetResponse(ctx, 403, `User ${ctx.authenticated.email} is not an admin, API access to addChannel denied.`, 'info')
     return
