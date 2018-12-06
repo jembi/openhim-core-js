4b82adaaa7ea7a2e9b13adff98f712ce093b9f42
Martin Brocker <martin.brocker@jembi.org>
2018-12-06 10:03:03 +0200

    Remove the load testing code run by Artillery.
    We have a better load testing framework thats done in the performance directory, so we have no need for this anymore

--- test/load-tests/env/clients.sh
+++
@@ -1,9 +0,0 @@
-node test/load-tests/env/dummy-endpoint-server.js &> /dev/null &&
-./test/load-tests/env/start-server.sh &&
-sleep 5 &&
-artillery run -o artillery/load-clients.json test/load-tests/clients.yml &&
-npm stop &&
-artillery report artillery/load-clients.json &&
-pkill -SIGINT clientLoadTest &&
-docker stop openhim-load-mongo &&
-docker rm openhim-load-mongo
