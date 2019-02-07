26b581370813f445fb1ee159d8fdfbdfc21670e4
brad <bradford.sawadye@jembi.org>
2019-02-07 16:36:07 +0200

    change the script of for building an rpm package and also update travis to build on node version 10.15.0
    THe builds fail on node version 10.15.1, so we are pinning this dependency in the rpm package
    
    OHM-751

--- packaging/build-docker-centos-rpm.sh
+++ packaging/build-docker-centos-rpm.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 RELEASE_VERSION=$1
 if [ -z ${RELEASE_VERSION} ]
 then
@@ -21,10 +23,10 @@ echo "Install needed packages: "
 docker exec -it $containerName sh -c "yum install -y git rpm-build redhat-rpm-config gcc-c++ make"
 
 echo "Install needed packages: "
-docker exec -it $containerName sh -c "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash"
+docker exec -it $containerName sh -c "curl -sL https://rpm.nodesource.com/setup_10.x | bash -"
 
 echo "Install needed packages: "
-docker exec -it $containerName sh -c "nvm install 10.15.0"
+docker exec -it $containerName sh -c "yum -y install nodejs-10.15.0"
 
 echo "Fetch release version from Github"
 docker exec -it $containerName sh -c "mkdir /openhim-core-js && curl -sL 'https://github.com/jembi/openhim-core-js/archive/v$RELEASE_VERSION.tar.gz' | tar --strip-components=1 -zxv -C /openhim-core-js"
