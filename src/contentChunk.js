20f408c3a10d6eb1320f54983cd5fbbb00a63ecd
Martin Brocker <martin.brocker@jembi.org>
2019-04-10 11:28:18 +0200

    WIP: Refactor function to return a promise instead of a callback
    Replaced creation of custom mongo connection with existing mongoose connection object
    
    OHM-762

--- src/contentChunk.js
+++ src/contentChunk.js
@@ -1,48 +1,22 @@
 
 import mongodb from 'mongodb'
-import { config } from './config'
+import { connectionDefault } from './config'
 
-const MongoClient = mongodb.MongoClient
-
-const db = null
-
-// WIP: TODO: Find better way to connect to mongoDB
-const getDB = async () => {
-  if (db) {
-    return db
-  }
-
-  try {
-    const client = await MongoClient.connect(config.mongo.url)
-    return client.db()
-  } catch (err) {
-    if (err) throw Error(err)
-    return
-  }
-}
-
-exports.extractPayloadIntoChunks = async (resource) => {
-  try {
-    const db = await getDB()
-
-    const bucket = new mongodb.GridFSBucket(db)
-
+exports.extractPayloadIntoChunks = (resource) => {
+  return new Promise((resolve, reject) => {
+    const bucket = new mongodb.GridFSBucket(connectionDefault.client.db())
     const stream = bucket.openUploadStream()
 
     stream.on('error', (err) => {
-      throw Error(err)
+      return reject(err)
     })
     .on('finish', (doc) => {
       if (!doc) {
-        throw new Error('GridFS create failed')
+        return reject('GridFS create failed')
       }
 
-      return doc._id
+      return resolve(doc._id)
     })
     stream.end(resource)
-  } catch (err) {
-    if (err) {
-      console.log('error: ', err)
-    }
-  }
+  })
 }
