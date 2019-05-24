176becfa2e5c521323ab1f30972cbc8f695bbcc9
Martin Brocker <martin.brocker@jembi.org>
2019-05-24 14:46:10 +0200

    Uncomment the test assert
    To ensure that test case is still passing as it did before
    
    OHM-776

--- test/integration/transactionsAPITests.js
+++ test/integration/transactionsAPITests.js
@@ -948,8 +948,8 @@ describe('API Integration Tests', () => {
         res.body[0].response.body.should.equal(`<HTTP body${TRUNCATE_APPEND}`)
 
         // Uncomment this when the functionality for retrieving the routes bodies is added
-        // res.body[0].routes[0].request.body.should.equal(`<HTTP body${TRUNCATE_APPEND}`)
-        // res.body[0].routes[0].response.body.should.equal(`<HTTP resp${TRUNCATE_APPEND}`)
+        res.body[0].routes[0].request.body.should.equal(`<HTTP body${TRUNCATE_APPEND}`)
+        res.body[0].routes[0].response.body.should.equal(`<HTTP body${TRUNCATE_APPEND}`)
 
         res.body[0].orchestrations[0].request.body.should.equal(`<HTTP body${TRUNCATE_APPEND}`)
         res.body[0].orchestrations[0].response.body.should.equal(`<HTTP body${TRUNCATE_APPEND}`)
