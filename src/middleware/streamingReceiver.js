efacb7e8a69bbf5ee4dc161a7ae96f1f31441601
Brent Engelbrecht <brent.engelbrecht@jembi.org>
2019-07-23 11:25:48 +0200

    Handle content matching in the streamingReceiver (old rawBodyParser)
    Prior to this change, matching on the body content of the request
    happened before the body was processed. With the addition of stream
    handling, the entire request body needs to be loaded in memory for
    string/regex scanning to be effective.
    
    This change creates two "modes" of reading in and processing the
    request:
    (1) fully-streamed
      - where each chunk of the request body is routed downstream as it
        is received
      - if there is a content-match (string/regex/json/xml), the full
        body is stored in GridFS and routed downstream (as a stream)
    (2) collected -
      - to enable string/regex scanning of the request body, all the
        body chunks are aggragated into a single buffer for scanning
      - If there is no content match after scanning, the transaction
        is de-authorised (and the middleware is completed; no storage
        to GridFS and no transaction is persisted)
    
    OHM-831

--- src/middleware/streamingReceiver.js
+++ src/middleware/streamingReceiver.js
@@ -2,15 +2,18 @@ import logger from 'winston'
 
 import * as messageStore from './messageStore'
 import { config } from '../config'
-import { Readable } from 'stream';
+import { Readable } from 'stream'
 import { getGridFSBucket }  from '../contentChunk'
 import { Types } from 'mongoose'
+import * as auditing from '../auditing'
+import { genAuthAudit } from './authorisation'
+import * as matching from './requestMatching'
 
 config.authentication = config.get('authentication')
 
 let bucket
 
-async function streamingReceiver (ctx) {
+function streamingReceiver (ctx, statusEvents) {
   let counter = 0
   let size = 0
 
@@ -23,10 +26,14 @@ async function streamingReceiver (ctx) {
 
   let gridFsStream
 
+  if (statusEvents && statusEvents.startRequest) {
+    statusEvents.startRequest(ctx.request.headers)
+  }
+
   /*
-   * Only transactions that were requested to be rerun should have this 
-   * custom header (the GridFS fileId of the body for this transaction)
-   */
+  * Only transactions that were requested to be rerun should have this
+  * custom header (the GridFS fileId of the body for this transaction)
+  */
   const bodyId = ctx.request.headers['x-body-id']
   const requestHasBody = (['POST', 'PUT', 'PATCH'].includes(ctx.req.method)) && (bodyId == null)
 
@@ -48,12 +55,14 @@ async function streamingReceiver (ctx) {
 
       gridFsStream
         .on('error', (err) => {
-          logger.error(`Couldn't stream request into GridFS for fileId: ${ctx.request.bodyId} - ${err}`)
+          if (statusEvents && statusEvents.gridFsError) {
+            statusEvents.gridFsError(err, ctx.request.bodyId)
+          }
         })
     } else {
       /*
-      *   Request has a bodyId (it's a rerun), so stream the body from GridFs 
-      *      and send it downstream
+      *   Request is a rerun, therefore has a bodyId, but no body.
+      *      So, stream the body from GridFs and send it downstream
       */
       const fileId = new Types.ObjectId(bodyId)
       gridFsStream = bucket.openDownloadStream(fileId)
@@ -70,13 +79,15 @@ async function streamingReceiver (ctx) {
           ctx.req.push(null)
         })
         .on('error', (err) => {
-          logger.error(`Cannot stream request body from GridFS for fileId: ${bodyId} - ${err}`)
+          if (statusEvents && statusEvents.gridFsError) {
+            statusEvents.gridFsError(err, bodyId)
+          }
         })
     }
   } else {
     /*
-     *  GET and DELETE come in here to persist the intial request transaction
-     */
+    *  GET and DELETE come in here to persist the intial request transaction
+    */
     ctx.state.requestPromise = messageStore.initiateRequest(ctx)
   }
 
@@ -90,32 +101,231 @@ async function streamingReceiver (ctx) {
       if (requestHasBody) {
         gridFsStream.write(chunk)
       }
+
       ctx.state.downstream.push(chunk)
     })
     .on('end', () => {
-      logger.info(`** END OF INPUT STREAM **`)
-
-      // Close streams to gridFS and downstream
       if (requestHasBody) {
+        // Close streams to gridFS and downstream
         gridFsStream.end()
+        if (statusEvents && statusEvents.finishGridFs) {
+          statusEvents.finishGridFs()
+        }
+
+        if (statusEvents && statusEvents.finishRequest) {
+          statusEvents.finishRequest()
+        }
       }
+
       ctx.state.downstream.push(null)
 
       // Update the transaction for Request (finished receiving)
       // Only update after `messageStore.initiateRequest` has completed
-      ctx.state.requestPromise.then(() => {
-        messageStore.completeRequest(ctx, () => {})
-      })
+      if (ctx.state.requestPromise) {
+        ctx.state.requestPromise.then(() => {
+          messageStore.completeRequest(ctx, () => {})
+        })
+      }
     })
     .on('error', (err) => {
-      logger.error(`Couldn't read request stream from socket: ${err}`)
+      if (statusEvents && statusEvents.requestError) {
+        statusEvents.requestError(err)
+      }
     })
+}
+
+async function collectingReceiver (ctx, statusEvents) {
+  return new Promise((resolve, reject) => {
+    let counter = 0
+    let size = 0
+    let bodyCopy = []
+
+    if (!bucket) {
+      bucket = getGridFSBucket()
+    }
+
+    ctx.state.downstream = new Readable()
+    ctx.state.downstream._read = () => {}
+
+    let gridFsStream
+    let allowRequest = true
+
+    /**
+     *  This event fires after the request headers are available,
+     *    but before the body has been received. By clearing the
+     *    ctx.authorisedChannel, the transaction will be de-authorised.
+     */
+    if (statusEvents && statusEvents.startRequest) {
+      const result = statusEvents.startRequest(ctx.request.headers)
+      if (result !== undefined) {
+        allowRequest = result
+      }
+      if (!allowRequest) {
+        ctx.authorisedChannel = null
+      }
+    }
+
+    /*
+    * Only transactions that were requested to be rerun should have this
+    * custom header (the GridFS fileId of the body for this transaction)
+    */
+    const bodyId = ctx.request.headers['x-body-id']
+    const requestHasBody = (['POST', 'PUT', 'PATCH'].includes(ctx.req.method)) && (bodyId == null)
+
+    if (allowRequest && !requestHasBody) {
+      /*
+      *   Request is a rerun, therefore has a bodyId, but no body.
+      *      So, stream the body from GridFs and send it into ctx.req
+      */
+      const fileId = new Types.ObjectId(bodyId)
+      gridFsStream = bucket.openDownloadStream(fileId)
+
+      ctx.request.bodyId = fileId
+      ctx.state.requestPromise = null
+
+      gridFsStream
+        .on('data', (chunk) => {
+          ctx.req.push(chunk)
+        })
+        .on('end', () => {
+          logger.info(`** END OF INPUT GRIDFS STREAM **`)
+          ctx.req.push(null)
+        })
+        .on('error', (err) => {
+          if (statusEvents && statusEvents.gridFsError) {
+            statusEvents.gridFsError(err, bodyId)
+          }
+          reject(err)
+        })
+    }
+
+    ctx.req
+      .on('data', (chunk) => {
+        if (allowRequest) {
+          counter++;
+          size += chunk.toString().length
+          logger.info(`Read request CHUNK # ${counter} [ Total size ${size}]`)
+
+          bodyCopy.push(chunk)
+          ctx.state.downstream.push(chunk)
+        }
+      })
+      .on('end', () => {
+        if (allowRequest) {
+          if (statusEvents && statusEvents.finishRequest) {
+            const result = statusEvents.finishRequest(Buffer.concat(bodyCopy).toString())
+            if (result !== undefined) {
+              allowRequest = result
+            }
+          }
+
+          ctx.state.downstream.push(null)
+
+          if (allowRequest) {
+            storeRequestAsString(Buffer.concat(bodyCopy).toString(), ctx.request, statusEvents)
+            ctx.state.requestPromise = messageStore.initiateRequest(ctx)
+            ctx.state.requestPromise.then(() => {
+              messageStore.completeRequest(ctx, () => {})
+            })
+          }
+
+          resolve()
+        }
+      })
+      .on('error', (err) => {
+        if (statusEvents && statusEvents.requestError) {
+          statusEvents.requestError(err)
+        }
+
+        reject(err)
+      })
+  })
+}
+
+export function storeRequestAsString (bodyString, request, statusEvents) {
+  if(!bucket) {
+    bucket = getGridFSBucket()
+  }
+
+  const uploadStream = bucket.openUploadStream()
+  request.bodyId = uploadStream.id
+
+  if (statusEvents.startGridFs) {
+    statusEvents.startGridFs(uploadStream.id)
+  }
+
+  uploadStream
+    .on('error', (err) => {
+      if (statusEvents.gridFsError) {
+        statusEvents.gridFsError(err)
+      }
+    })
+    .on('finish', (fileId) => {
+      if (statusEvents.finishGridFs) {
+        statusEvents.finishGridFs(fileId)
+      }
+    })
+
+  uploadStream.write(bodyString)
+  uploadStream.end()
 }
 
 /*
  * Koa middleware for streaming to GridFS and streaming routing
  */
 export async function koaMiddleware (ctx, next) {
-  streamingReceiver(ctx)
-  await next()
+
+  let channel = ctx.authorisedChannel || null
+  let collectBody = false
+
+  const statusEvents = {
+    startRequest: function (headers) {},
+    finishRequest: function (body) {
+      logger.info(`** END OF INPUT STREAM **`)
+      if (!collectBody) {
+        return true
+      }
+
+      const isMatched = matching.matchContent(body, ctx.authorisedChannel)
+      if (!isMatched) {
+        ctx.authorisedChannel = null
+        ctx.response.status = 401
+        if (config.authentication.enableBasicAuthentication) {
+          ctx.set('WWW-Authenticate', 'Basic')
+        }
+        logger.info(`The request, '${ctx.request.path}', access to channel revoked (no content match).`)
+        auditing.sendAuditEvent(genAuthAudit(ctx.ip), () => logger.debug('Processed nodeAuthentication audit'))
+      }
+      return isMatched
+    },
+    requestError: function (err) {
+      logger.error(`Couldn't read request stream from socket: ${err}`)
+    },
+    startGridFs: function (bodyId) {},
+    finishGridFs: function () {},
+    gridFsError: function (err, bodyId) {
+      logger.error(`GridFS streaming error for bodyId: ${bodyId} - ${err}`)
+    }
+  }
+
+  if (channel) {
+    collectBody = (channel.matchContentRegex !== null ||
+      channel.matchContentXpath !== null ||
+      channel.matchContentValue !== null ||
+      channel.matchContentJson !== null)
+  }
+
+  if (collectBody && ['POST', 'PUT', 'PATCH'].includes(ctx.req.method)) {
+    try {
+      await collectingReceiver(ctx, statusEvents)
+    } catch(err) {
+      logger.error(`collectingReceiver error: ${err}`)
+    }
+  } else {
+    streamingReceiver(ctx, statusEvents)
+  }
+
+  if (ctx.authorisedChannel) {
+    await next()
+  }
 }
