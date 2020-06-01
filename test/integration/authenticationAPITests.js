b11f7e27e4ae13e194cf92035abea69b5fe15e51
MattyJ007 <matthew.dickie@jembi.org>
2020-06-01 16:10:52 +0200

    Format Auth Integration test file
    The code was inconsistent and there were some floating brackets...
    
    TRACE-131

--- test/integration/authenticationAPITests.js
+++ test/integration/authenticationAPITests.js
@@ -52,13 +52,17 @@ describe('API Integration Tests', () => {
         .set('auth-token', authDetails.authToken)
         .expect(200)
 
-      await testUtils.pollCondition(() => AuditModel.countDocuments().then(c => c === 1))
+      await testUtils.pollCondition(() =>
+        AuditModel.countDocuments().then((c) => c === 1)
+      )
       const audits = await AuditModel.find()
 
       audits.length.should.be.exactly(1)
       audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('0') // success
       audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
-      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
+      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal(
+        'Login'
+      )
       audits[0].activeParticipant.length.should.be.exactly(2)
       audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
       audits[0].activeParticipant[1].userID.should.be.equal('root@jembi.org')
@@ -67,16 +71,25 @@ describe('API Integration Tests', () => {
     it('should audit a successful login on an API endpoint with basic auth', async () => {
       await request(constants.BASE_URL)
         .get('/channels')
-        .set('Authorization', `Basic ${Buffer.from(`${testUtils.rootUser.email}:password`).toString('base64')}`)
+        .set(
+          'Authorization',
+          `Basic ${Buffer.from(`${testUtils.rootUser.email}:password`).toString(
+            'base64'
+          )}`
+        )
         .expect(200)
 
-      await testUtils.pollCondition(() => AuditModel.countDocuments().then(c => c === 1))
+      await testUtils.pollCondition(() =>
+        AuditModel.countDocuments().then((c) => c === 1)
+      )
       const audits = await AuditModel.find()
 
       audits.length.should.be.exactly(1)
       audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('0') // success
       audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
-      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
+      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal(
+        'Login'
+      )
       audits[0].activeParticipant.length.should.be.exactly(2)
       audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
       audits[0].activeParticipant[1].userID.should.be.equal('root@jembi.org')
@@ -91,13 +104,17 @@ describe('API Integration Tests', () => {
         .set('auth-token', authDetails.authToken)
         .expect(401)
 
-      await testUtils.pollCondition(() => AuditModel.countDocuments().then(c => c === 1))
+      await testUtils.pollCondition(() =>
+        AuditModel.countDocuments().then((c) => c === 1)
+      )
       const audits = await AuditModel.find({})
 
       audits.length.should.be.exactly(1)
       audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('8') // failure
       audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
-      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
+      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal(
+        'Login'
+      )
       audits[0].activeParticipant.length.should.be.exactly(2)
       audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
       audits[0].activeParticipant[1].userID.should.be.equal('wrong@email.org')
@@ -106,16 +123,23 @@ describe('API Integration Tests', () => {
     it('should audit an unsuccessful login on an API endpoint with basic auth and incorrect email', async () => {
       await request(constants.BASE_URL)
         .get('/channels')
-        .set('Authorization', `Basic ${Buffer.from(`wrong@email.org:password`).toString('base64')}`)
+        .set(
+          'Authorization',
+          `Basic ${Buffer.from(`wrong@email.org:password`).toString('base64')}`
+        )
         .expect(401)
 
-      await testUtils.pollCondition(() => AuditModel.countDocuments().then(c => c === 1))
+      await testUtils.pollCondition(() =>
+        AuditModel.countDocuments().then((c) => c === 1)
+      )
       const audits = await AuditModel.find({})
 
       audits.length.should.be.exactly(1)
       audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('8') // failure
       audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
-      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
+      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal(
+        'Login'
+      )
       audits[0].activeParticipant.length.should.be.exactly(2)
       audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
       audits[0].activeParticipant[1].userID.should.be.equal('wrong@email.org')
@@ -124,16 +148,25 @@ describe('API Integration Tests', () => {
     it('should audit an unsuccessful login on an API endpoint with basic auth and incorrect password', async () => {
       await request(constants.BASE_URL)
         .get('/channels')
-        .set('Authorization', `Basic ${Buffer.from(`${testUtils.rootUser.email}:drowssap`).toString('base64')}`)
+        .set(
+          'Authorization',
+          `Basic ${Buffer.from(`${testUtils.rootUser.email}:drowssap`).toString(
+            'base64'
+          )}`
+        )
         .expect(401)
 
-      await testUtils.pollCondition(() => AuditModel.countDocuments().then(c => c === 1))
+      await testUtils.pollCondition(() =>
+        AuditModel.countDocuments().then((c) => c === 1)
+      )
       const audits = await AuditModel.find({})
 
       audits.length.should.be.exactly(1)
       audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('8') // failure
       audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
-      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
+      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal(
+        'Login'
+      )
       audits[0].activeParticipant.length.should.be.exactly(2)
       audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
       audits[0].activeParticipant[1].userID.should.be.equal('root@jembi.org')
@@ -160,13 +193,17 @@ describe('API Integration Tests', () => {
         .set('auth-token', authDetails.authToken)
         .expect(401)
 
-      await testUtils.pollCondition(() => AuditModel.countDocuments().then(c => c === 1))
+      await testUtils.pollCondition(() =>
+        AuditModel.countDocuments().then((c) => c === 1)
+      )
       const audits = await AuditModel.find({})
 
       audits.length.should.be.exactly(1)
       audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('8') // failure
       audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
-      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
+      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal(
+        'Login'
+      )
       audits[0].activeParticipant.length.should.be.exactly(2)
       audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
       audits[0].activeParticipant[1].userID.should.be.equal('wrong@email.org')
@@ -196,7 +233,9 @@ describe('API Integration Tests', () => {
       audits.length.should.be.exactly(1)
       audits[0].eventIdentification.eventOutcomeIndicator.should.be.equal('0') // success
       audits[0].eventIdentification.eventTypeCode.code.should.be.equal('110122')
-      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal('Login')
+      audits[0].eventIdentification.eventTypeCode.displayName.should.be.equal(
+        'Login'
+      )
       audits[0].activeParticipant.length.should.be.exactly(2)
       audits[0].activeParticipant[0].userID.should.be.equal('OpenHIM')
       audits[0].activeParticipant[1].userID.should.be.equal('root@jembi.org')
@@ -216,12 +255,14 @@ describe('API Integration Tests', () => {
           name: 'TEST DATA - Mock endpoint',
           urlPattern: 'test/mock',
           allow: ['PoC'],
-          routes: [{
-            name: 'test route',
-            host: 'localhost',
-            port: constants.MEDIATOR_PORT,
-            primary: true
-          }],
+          routes: [
+            {
+              name: 'test route',
+              host: 'localhost',
+              port: constants.MEDIATOR_PORT,
+              primary: true
+            }
+          ],
           updatedBy: {
             id: new ObjectId(),
             name: 'Test'
@@ -232,24 +273,20 @@ describe('API Integration Tests', () => {
           clientID: 'testApp',
           clientDomain: 'test-client.jembi.org',
           name: 'TEST Client',
-          roles: [
-            'OpenMRS_PoC',
-            'PoC'
-          ],
+          roles: ['OpenMRS_PoC', 'PoC'],
           passwordHash: '',
-          certFingerprint: '6D:BF:A5:BE:D7:F5:01:C2:EC:D0:BC:74:A4:12:5A:6F:36:C4:77:5C'
+          certFingerprint:
+            '6D:BF:A5:BE:D7:F5:01:C2:EC:D0:BC:74:A4:12:5A:6F:36:C4:77:5C'
         }
 
         const testClientDoc2 = {
           clientID: 'testApp2',
           clientDomain: 'ca.openhim.org',
           name: 'TEST Client 2',
-          roles: [
-            'OpenMRS_PoC',
-            'PoC'
-          ],
+          roles: ['OpenMRS_PoC', 'PoC'],
           passwordHash: '',
-          certFingerprint: '6B:0D:BD:02:BB:A4:40:29:89:51:6A:0A:A2:F4:BD:8B:F8:E8:47:84'
+          certFingerprint:
+            '6B:0D:BD:02:BB:A4:40:29:89:51:6A:0A:A2:F4:BD:8B:F8:E8:47:84'
         }
         await ClientModelAPI.deleteMany({})
 
@@ -263,24 +300,37 @@ describe('API Integration Tests', () => {
           key: fs.readFileSync('test/resources/server-tls/key.pem'),
           cert: {
             data: fs.readFileSync('test/resources/server-tls/cert.pem'),
-            fingerprint: '23:37:6A:5E:A9:13:A4:8C:66:C5:BB:9F:0E:0D:68:9B:99:80:10:FC'
+            fingerprint:
+              '23:37:6A:5E:A9:13:A4:8C:66:C5:BB:9F:0E:0D:68:9B:99:80:10:FC'
           },
-          ca: [{
-            data: fs.readFileSync('test/resources/client-tls/cert.pem'),
-            fingerprint: '6D:BF:A5:BE:D7:F5:01:C2:EC:D0:BC:74:A4:12:5A:6F:36:C4:77:5C'
-          },
-          {
-            data: fs.readFileSync('test/resources/trust-tls/chain/intermediate.cert.pem'),
-            fingerprint: 'A9:C5:37:DF:84:FA:C8:BD:B8:5F:A3:9B:FF:52:D0:DB:79:9F:B1:3C'
-          },
-          {
-            data: fs.readFileSync('test/resources/trust-tls/chain/ca.cert.pem'),
-            fingerprint: '6B:0D:BD:02:BB:A4:40:29:89:51:6A:0A:A2:F4:BD:8B:F8:E8:47:84'
-          }
+          ca: [
+            {
+              data: fs.readFileSync('test/resources/client-tls/cert.pem'),
+              fingerprint:
+                '6D:BF:A5:BE:D7:F5:01:C2:EC:D0:BC:74:A4:12:5A:6F:36:C4:77:5C'
+            },
+            {
+              data: fs.readFileSync(
+                'test/resources/trust-tls/chain/intermediate.cert.pem'
+              ),
+              fingerprint:
+                'A9:C5:37:DF:84:FA:C8:BD:B8:5F:A3:9B:FF:52:D0:DB:79:9F:B1:3C'
+            },
+            {
+              data: fs.readFileSync(
+                'test/resources/trust-tls/chain/ca.cert.pem'
+              ),
+              fingerprint:
+                '6B:0D:BD:02:BB:A4:40:29:89:51:6A:0A:A2:F4:BD:8B:F8:E8:47:84'
+            }
           ]
         }).save()
 
-        mockServer = await testUtils.createMockHttpServer('Mock response body\n', constants.MEDIATOR_PORT, 201)
+        mockServer = await testUtils.createMockHttpServer(
+          'Mock response body\n',
+          constants.MEDIATOR_PORT,
+          201
+        )
       })
 
       after(async () => {
@@ -297,7 +347,10 @@ describe('API Integration Tests', () => {
       })
 
       it('should forward a request to the configured routes if the client is authenticated and authorised', async () => {
-        await promisify(server.start)({ httpPort: SERVER_PORTS.httpPort, httpsPort: SERVER_PORTS.httpsPort })
+        await promisify(server.start)({
+          httpPort: SERVER_PORTS.httpPort,
+          httpsPort: SERVER_PORTS.httpsPort
+        })
 
         const options = {
           host: 'localhost',
@@ -320,7 +373,10 @@ describe('API Integration Tests', () => {
       })
 
       it('should reject a request when using an invalid cert', async () => {
-        await promisify(server.start)({ httpPort: SERVER_PORTS.httpPort, httpsPort: SERVER_PORTS.httpsPort })
+        await promisify(server.start)({
+          httpPort: SERVER_PORTS.httpPort,
+          httpsPort: SERVER_PORTS.httpsPort
+        })
 
         const options = {
           host: 'localhost',
@@ -342,15 +398,22 @@ describe('API Integration Tests', () => {
         })
       })
 
-      it('should authenticate a client further up the chain if \'in-chain\' config is set', async () => {
+      it("should authenticate a client further up the chain if 'in-chain' config is set", async () => {
         config.tlsClientLookup.type = 'in-chain'
-        await promisify(server.start)({ httpPort: SERVER_PORTS.httpPort, httpsPort: SERVER_PORTS.httpsPort })
+        await promisify(server.start)({
+          httpPort: SERVER_PORTS.httpPort,
+          httpsPort: SERVER_PORTS.httpsPort
+        })
         const options = {
           host: 'localhost',
           path: '/test/mock',
           port: SERVER_PORTS.httpsPort,
-          cert: fs.readFileSync('test/resources/trust-tls/chain/test.openhim.org.cert.pem'),
-          key: fs.readFileSync('test/resources/trust-tls/chain/test.openhim.org.key.pem'),
+          cert: fs.readFileSync(
+            'test/resources/trust-tls/chain/test.openhim.org.cert.pem'
+          ),
+          key: fs.readFileSync(
+            'test/resources/trust-tls/chain/test.openhim.org.key.pem'
+          ),
           ca: [fs.readFileSync('test/resources/server-tls/cert.pem')]
         }
 
@@ -365,9 +428,12 @@ describe('API Integration Tests', () => {
         })
       })
 
-      it('should reject a request with an invalid cert if \'in-chain\' config is set', async () => {
+      it("should reject a request with an invalid cert if 'in-chain' config is set", async () => {
         config.tlsClientLookup.type = 'in-chain'
-        await promisify(server.start)({ httpPort: SERVER_PORTS.httpPort, httpsPort: SERVER_PORTS.httpsPort })
+        await promisify(server.start)({
+          httpPort: SERVER_PORTS.httpPort,
+          httpsPort: SERVER_PORTS.httpsPort
+        })
 
         const options = {
           host: 'localhost',
@@ -389,16 +455,23 @@ describe('API Integration Tests', () => {
         })
       })
 
-      it('should NOT authenticate a client further up the chain if \'strict\' config is set', async () => {
+      it("should NOT authenticate a client further up the chain if 'strict' config is set", async () => {
         config.tlsClientLookup.type = 'strict'
-        await promisify(server.start)({ httpPort: SERVER_PORTS.httpPort, httpsPort: SERVER_PORTS.httpsPort })
+        await promisify(server.start)({
+          httpPort: SERVER_PORTS.httpPort,
+          httpsPort: SERVER_PORTS.httpsPort
+        })
 
         const options = {
           host: 'localhost',
           path: '/test/mock',
           port: SERVER_PORTS.httpsPort,
-          cert: fs.readFileSync('test/resources/trust-tls/chain/test.openhim.org.cert.pem'),
-          key: fs.readFileSync('test/resources/trust-tls/chain/test.openhim.org.key.pem'),
+          cert: fs.readFileSync(
+            'test/resources/trust-tls/chain/test.openhim.org.cert.pem'
+          ),
+          key: fs.readFileSync(
+            'test/resources/trust-tls/chain/test.openhim.org.key.pem'
+          ),
           ca: [fs.readFileSync('test/resources/server-tls/cert.pem')]
         }
 
@@ -426,12 +499,14 @@ describe('API Integration Tests', () => {
           name: 'TEST DATA - Mock endpoint',
           urlPattern: 'test/mock',
           allow: ['PoC'],
-          routes: [{
-            name: 'test route',
-            host: 'localhost',
-            port: constants.MEDIATOR_PORT,
-            primary: true
-          }],
+          routes: [
+            {
+              name: 'test route',
+              host: 'localhost',
+              port: constants.MEDIATOR_PORT,
+              primary: true
+            }
+          ],
           updatedBy: {
             id: new ObjectId(),
             name: 'Test'
@@ -442,17 +517,19 @@ describe('API Integration Tests', () => {
           clientID: 'testApp',
           clientDomain: 'openhim.jembi.org',
           name: 'TEST Client',
-          roles: [
-            'OpenMRS_PoC',
-            'PoC'
-          ],
+          roles: ['OpenMRS_PoC', 'PoC'],
           passwordAlgorithm: 'bcrypt',
-          passwordHash: '$2a$10$w8GyqInkl72LMIQNpMM/fenF6VsVukyya.c6fh/GRtrKq05C2.Zgy',
+          passwordHash:
+            '$2a$10$w8GyqInkl72LMIQNpMM/fenF6VsVukyya.c6fh/GRtrKq05C2.Zgy',
           cert: ''
         }
 
         new ClientModelAPI(testAppDoc).save()
-        mockServer = await testUtils.createMockHttpServer('Mock response body 1\n', constants.MEDIATOR_PORT, 200)
+        mockServer = await testUtils.createMockHttpServer(
+          'Mock response body 1\n',
+          constants.MEDIATOR_PORT,
+          200
+        )
       })
 
       after(async () => {
@@ -467,38 +544,30 @@ describe('API Integration Tests', () => {
         await promisify(server.stop)()
       })
 
-      describe('with no credentials', () =>
-        it('should `throw` 401', async () => {
-          await promisify(server.start)({ httpPort: SERVER_PORTS.httpPort })
+      it('should `throw` 401 when no credentials provided', async () => {
+        await promisify(server.start)({ httpPort: SERVER_PORTS.httpPort })
 
-          await request(constants.HTTP_BASE_URL)
-            .get('/test/mock')
-            .expect(401)
-        })
-      )
+        await request(constants.HTTP_BASE_URL).get('/test/mock').expect(401)
+      })
 
-      describe('with incorrect credentials', () =>
-        it('should `throw` 401', async () => {
-          await promisify(server.start)({ httpPort: SERVER_PORTS.httpPort })
+      it('should `throw` 401 when incorrect details provided', async () => {
+        await promisify(server.start)({ httpPort: SERVER_PORTS.httpPort })
 
-          await request(constants.HTTP_BASE_URL)
-            .get('/test/mock')
-            .auth('incorrect_user', 'incorrect_password')
-            .expect(401)
-            .expect('WWW-Authenticate', 'Basic')
-        })
-      )
+        await request(constants.HTTP_BASE_URL)
+          .get('/test/mock')
+          .auth('incorrect_user', 'incorrect_password')
+          .expect(401)
+          .expect('WWW-Authenticate', 'Basic')
+      })
 
-      describe('with correct credentials', () =>
-        it('should return 200 OK', async () => {
-          await promisify(server.start)({ httpPort: SERVER_PORTS.httpPort })
+      it('should return 200 OK with correct credentials', async () => {
+        await promisify(server.start)({ httpPort: SERVER_PORTS.httpPort })
 
-          await request(constants.HTTP_BASE_URL)
-            .get('/test/mock')
-            .auth('testApp', 'password')
-            .expect(200)
-        })
-      )
+        await request(constants.HTTP_BASE_URL)
+          .get('/test/mock')
+          .auth('testApp', 'password')
+          .expect(200)
+      })
     })
   })
 })
