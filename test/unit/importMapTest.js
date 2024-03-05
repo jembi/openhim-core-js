bde0ed212e3cbc4d4ed501dc57f7fb211c0ae537
ItsMurumba <kevmurumba@gmail.com>
2024-03-05 11:50:39 +0300

    Adds more test scenarios to the ImportMapTest unit test

--- test/unit/importMapTest.js
+++ test/unit/importMapTest.js
@@ -9,6 +9,7 @@ import {AppModel} from '../../src/model/apps'
 describe('setupAppChangeStream', () => {
   let appModelWatchStub
   let importMapModelStub
+  let consoleLogStub
 
   beforeEach(() => {
     appModelWatchStub = sinon.stub(AppModel, 'watch').returns({
@@ -21,10 +22,69 @@ describe('setupAppChangeStream', () => {
   afterEach(() => {
     appModelWatchStub.restore()
     importMapModelStub.restore()
+    consoleLogStub.restore()
   })
 
   it('should call AppModel.watch', () => {
     setupAppChangeStream()
     should.exist(appModelWatchStub.calledOnce)
+  })
+
+  it('should update ImportMapModel on insert operation', async () => {
+    const change = {
+      operationType: 'insert',
+      fullDocument: {_id: 'id', name: 'name', url: 'url'}
+    }
+
+    setupAppChangeStream()
+    const changeCallback = appModelWatchStub.firstCall.args[0]
+
+    await changeCallback(change)
+
+    importMapModelStub.updateOne.calledWith(
+      {appId: 'id', name: 'name'},
+      {url: 'url'},
+      {upsert: true}
+    ).should.be.true
+  })
+
+  it('should update ImportMapModel on update operation', async () => {
+    const change = {
+      operationType: 'update',
+      fullDocument: {_id: 'id', name: 'name', url: 'url'}
+    }
+
+    setupAppChangeStream()
+    const changeCallback = appModelWatchStub.firstCall.args[0]
+
+    await changeCallback(change)
+
+    importMapModelStub.updateOne.calledWith(
+      {appId: 'id', name: 'name'},
+      {url: 'url'}
+    ).should.be.true
+  })
+
+  it('should delete from ImportMapModel on delete operation', async () => {
+    const change = {operationType: 'delete', documentKey: {_id: 'id'}}
+
+    setupAppChangeStream()
+    const changeCallback = appModelWatchStub.firstCall.args[0]
+
+    await changeCallback(change)
+
+    importMapModelStub.deleteOne.calledWith({appId: 'id'}).should.be.true
+  })
+
+  it('should log unsupported operation type', async () => {
+    const change = {operationType: 'unsupported'}
+
+    setupAppChangeStream()
+    const changeCallback = appModelWatchStub.firstCall.args[0]
+
+    await changeCallback(change)
+
+    consoleLogStub.calledWith('Unsupported operation type:', 'unsupported')
+      .should.be.true
   })
 })
