b16a2a04e338720a8b4759c4ff4f5e24d0d5a121
drono <derrick.rono@jembi.org>
2024-09-02 12:54:59 +0300

    update the default import map paths

--- src/constants.js
+++ src/constants.js
@@ -18,21 +18,23 @@ export const JWT_PATTERN =
 export const CUSTOM_TOKEN_PATTERN =
   /^ *(?:[Cc][Uu][Ss][Tt][Oo][Mm]) +([A-Za-z0-9\-._~+/]+=*) *$/
 
-const OPENHIM_CONSOLE_BASE_URL = config.get('openhimConsoleBaseUrl') 
+const OPENHIM_CONSOLE_BASE_URL = config.get('openhimConsoleBaseUrl')
 
 export const DEFAULT_IMPORT_MAP_PATHS = {
-  '@jembi/openhim-header':
-    `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/openhim-header/dist/jembi-openhim-header.js`,
+  '@jembi/openhim-header': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/openhim-header/dist/jembi-openhim-header.js`,
   '@jembi/legacy-console': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/legacy-console/dist/bundle.js`,
-  '@jembi/openhim-core-api':
-  `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/openhim-core-api/dist/jembi-openhim-core-api.js`,
-  '@jembi/openhim-theme':
-  `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/openhim-theme/dist/jembi-openhim-theme.js`,
-  '@jembi/portal-admin':
-  `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/portal-admin/dist/jembi-portal-admin.js`,
-  '@jembi/openhim-portal':
-  `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/openhim-portal/dist/jembi-openhim-portal.js`,
+  '@jembi/openhim-core-api': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/openhim-core-api/dist/jembi-openhim-core-api.js`,
+  '@jembi/openhim-theme': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/openhim-theme/dist/jembi-openhim-theme.js`,
+  '@jembi/portal-admin': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/portal-admin/dist/jembi-portal-admin.js`,
+  '@jembi/openhim-portal': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/openhim-portal/dist/jembi-openhim-portal.js`,
   '@jembi/root-config': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/root-config/dist/jembi-root-config.js`,
-  '@jembi/openhim-sidebar':
-  `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/openhim-sidebar/dist/jembi-openhim-sidebar.js`
+  '@jembi/openhim-sidebar': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/openhim-sidebar/dist/jembi-openhim-sidebar.js`,
+  '@jembi/clients-app': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/clients-app/dist/jembi-clients-app.js`,
+  '@jembi/dashboard-app': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/dashboard-app/dist/jembi-dashboard-app.js`,
+  '@jembi/rbac-app': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/rbac-app/dist/jembi-rbac-app.js`,
+  '@jembi/footer-app': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/footer-app/dist/jembi-footer-app.js`,
+  '@jembi/transaction-log': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/transaction-log/dist/jembi-transaction-log.js`,
+  '@jembi/channels-app': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/channels-app/dist/jembi-channels-app.js`,
+  '@jembi/users-app': `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/users-app/dist/jembi-users-app.js`,
+  "@jembi/client-roles-app": `${OPENHIM_CONSOLE_BASE_URL}/libs/@jembi/client-roles-app/dist/jembi-client-roles-app.js`  
 }
