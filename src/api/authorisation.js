8c5e9ca17732c8b6a1280d5aae1be24ca99e76d0
bradsawadye <bradsawadye@gmail.com>
2024-09-04 12:31:28 +0200

    refactor

--- src/api/authorisation.js
+++ src/api/authorisation.js
@@ -7,21 +7,24 @@ export function inGroup(group, user) {
   return user.groups.indexOf(group) >= 0
 }
 
+const getUserChannelsByPermissions = (user, allPermission, specifiedPermission) =>
+  RoleModelAPI.find({name: {$in: user.groups}}, {permissions: {[allPermission]: 1, [specifiedPermission]: 1}}).then(roles => {
+    if (roles.find(role => role.permissions[allPermission] || role.permissions[allPermission.replace('view', 'manage')])) {
+      return ChannelModelAPI.find({}).exec()
+    }
+    const specifiedChannels = roles.reduce((prev, curr) =>
+      prev.concat(curr.permissions[specifiedPermission], curr.permissions[specifiedPermission.replace('view', 'manage')]),
+      []
+    )
+    return ChannelModelAPI.find({_id: {$in: specifiedChannels}}).exec()
+  })
+
 /**
  * A promise returning function that returns the list
  * of viewable channels for a user.
  */
 export function getUserViewableChannels(user) {
-  return RoleModelAPI.find({name: {$in: user.groups}}, {permissions: {"channel-view-all": 1, "channel-view-specified": 1}}).then(roles => {
-    if (roles.find(role => role.permissions['channel-view-all'])) {
-      return ChannelModelAPI.find({}).exec()
-    }
-    const specifiedChannels = roles.reduce((prev, curr) =>
-      prev.concat(curr.permissions['channel-view-specified']),
-      []
-    )
-    return ChannelModelAPI.find({_id: {$in: specifiedChannels}}).exec()
-  })
+  return getUserChannelsByPermissions(user, 'channel-view-all', 'channel-view-specified')
 }
 
 /**
@@ -29,14 +32,5 @@ export function getUserViewableChannels(user) {
  * of rerunnable channels for a user.
  */
 export function getUserRerunableChannels(user) {
-  return RoleModelAPI.find({name: {$in: user.groups}}, {permissions: {"transaction-rerun-all": 1, "transaction-rerun-specified": 1}}).then(roles => {
-    if (roles.find(role => role.permissions['transaction-rerun-all'])) {
-      return ChannelModelAPI.find({}).exec()
-    }
-    const specifiedChannels = roles.reduce((prev, curr) =>
-      prev.concat(curr.permissions['transaction-rerun-specified']),
-      []
-    )
-    return ChannelModelAPI.find({_id: {$in: specifiedChannels}}).exec()
-  })
+  return getUserChannelsByPermissions(user, 'transaction-rerun-all', 'transaction-rerun-specified')
 }
